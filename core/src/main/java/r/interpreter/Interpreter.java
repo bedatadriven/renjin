4869e48f23e21eaca8de6ca5c508059158ebbab5
alex@bedatadriven.com <alex@bedatadriven.com@b722d886-ae41-2056-5ac6-6db9963f4c99>
2010-10-24 19:37:34 +0000

    Committing missing files
    git-svn-id: https://renjin.googlecode.com/svn/trunk@8 b722d886-ae41-2056-5ac6-6db9963f4c99

---
+++ core/src/main/java/r/interpreter/Interpreter.java
@@ -0,0 +1,110 @@
+/*
+ * R : A Computer Language for Statistical Data Analysis
+ * Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
+ * Copyright (C) 1997--2008  The R Development Core Team
+ * Copyright (C) 2003, 2004  The R Foundation
+ * Copyright (C) 2010 bedatadriven
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package r.interpreter;
+
+import r.lang.EvalResult;
+import r.lang.GlobalContext;
+import r.lang.SEXP;
+import r.lang.exception.EvalException;
+import r.lang.exception.FunctionCallException;
+import r.lang.exception.ParseException;
+import r.parser.ParseOptions;
+import r.parser.ParseState;
+import r.parser.RLexer;
+import r.parser.RParser;
+
+import java.io.Reader;
+
+/**
+ * Interactive interpreter for the R-language
+ */
+public class Interpreter implements Runnable {
+
+  private final Console console;
+  private GlobalContext global;
+
+  public Interpreter(Console console) {
+    this.global = new GlobalContext();
+    this.console = console;
+    if(console instanceof RichConsole) {
+      ((RichConsole) console).setNameCompletion(new SymbolCompletion(global));
+    }
+  }
+
+  @Override
+  public void run() {
+
+    ParseOptions options = ParseOptions.defaults();
+    ParseState state = new ParseState();
+    Reader reader = console.getIn();
+    RLexer lexer = new RLexer(global, options, state, reader);
+    RParser parser = new RParser(options, state, global, lexer);
+
+    printGreeting();
+
+    while(true) {
+
+      console.print("> ");
+
+      try {
+        parser.parse();
+
+        SEXP exp = parser.getResult();
+        EvalResult result = exp.evaluate(global.getGlobalEnvironment());
+
+        if(result.isVisible()) {
+          console.println(result.getExpression());
+        }
+
+      } catch (ParseException e) {
+        console.println(String.format("Error: %s", e.getMessage()));
+
+      } catch (FunctionCallException e) {
+        console.println(String.format("Error: %s", e.getMessage()));
+
+      } catch (EvalException e) {
+        console.println(String.format("Error: %s", e.getMessage()));
+
+      } catch (Exception e) {
+        console.println(String.format("Java exception thrown: " + e.getMessage()));
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private void printGreeting() {
+    console.print("\nCopyright (C) 2010 The R Foundation for Statistical Computing\n");
+    console.print("ISBN 3-900051-07-0\n\n");
+
+    console.print("R is free software and comes with ABSOLUTELY NO WARRANTY. " +
+      "You are welcome to redistribute it under certain conditions. " +
+      "Type 'license()' or 'licence()' for distribution details.\n\n");
+
+    console.print("R is a collaborative project with many contributors. " +
+      "Type 'contributors()' for more information and " +
+      "'citation()' on how to cite R or R packages in publications.\n\n");
+
+    console.print("Type 'demo()' for some demos, 'help()' for on-line help, or" +
+      "'help.start()' for an HTML browser interface to help." +
+      "Type 'q()' to quit R.\n\n");
+  }
+}

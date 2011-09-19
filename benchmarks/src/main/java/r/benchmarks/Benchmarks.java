7911ea3556d908ec3cbde4225731d1724c2ce3a8
alex@bedatadriven.com <alex@bedatadriven.com@b722d886-ae41-2056-5ac6-6db9963f4c99>
2011-09-19 21:25:19 +0000

    Added first benchmark to what will hopefully become a comprehensive benchmark suite.
    This particular benchmark is from TU dortmund -- renjin now looks to be
    currently about 2.5x slower than R 2.12. (This is down from 40 x slower !!)
    
    
    
    git-svn-id: https://renjin.googlecode.com/svn/trunk@323 b722d886-ae41-2056-5ac6-6db9963f4c99

---
+++ benchmarks/src/main/java/r/benchmarks/Benchmarks.java
@@ -0,0 +1,48 @@
+package r.benchmarks;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import r.lang.Context;
+import r.lang.EvalResult;
+import r.lang.SEXP;
+import r.parser.RParser;
+
+public class Benchmarks {
+  
+  
+  
+  public static void main(String[] args) throws IOException {
+  
+    long startUpTime = System.nanoTime();
+    
+    
+    Context topLevelContext = Context.newTopLevelContext();
+    topLevelContext.init();
+    
+    long afterInit = System.nanoTime();
+    
+    InputStream in = Benchmarks.class.getResourceAsStream("mean-var-online.R");
+    SEXP source = RParser.parseSource(new InputStreamReader(in));
+    
+    long afterParse = System.nanoTime();
+    
+    EvalResult result = source.evaluate(topLevelContext, topLevelContext.getEnvironment());
+    System.out.println( result );
+    
+    long afterEvaluate = System.nanoTime();
+    
+
+    System.out.println("Total time:\t" + formatNanos(afterEvaluate - startUpTime));
+    System.out.println("Parse time:\t" + formatNanos(afterParse - startUpTime));
+
+    System.out.println("Eval time:\t" + formatNanos(afterEvaluate - afterParse));
+        
+  }
+
+  private static String formatNanos(long nanos) {
+    return (nanos * 1e-9) + " s";
+  }
+  
+}

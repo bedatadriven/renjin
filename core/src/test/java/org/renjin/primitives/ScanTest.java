e6a03c62ed614c8c330c284728740ed71d908837
Alex Bertram <alex@bedatadriven.com>
2013-02-17 22:32:55 +0100

    Checks in unit test for Issue #22

---
+++ core/src/test/java/org/renjin/primitives/ScanTest.java
@@ -0,0 +1,22 @@
+package org.renjin.primitives;
+
+import org.junit.Test;
+import org.renjin.EvalTestCase;
+import org.renjin.sexp.StringVector;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class ScanTest extends EvalTestCase {
+
+  @Test
+  public void issue22() {
+    
+    global.setVariable("file.name", StringVector.valueOf( getClass().getResource("/scantest.txt").getFile()) );
+    eval("x <- scan(file.name, nlines=200, comment.char='#')");
+    eval("print(x)" );
+    assertThat( eval("length(x)"), equalTo(c_i(585)));
+    
+  }
+  
+}

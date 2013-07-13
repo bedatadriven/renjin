f6cb0d2c674e9c59516d0971e4976fb748a75971
Alexander Bertram <alex@bedatadriven.com>
2013-07-13 23:04:32 +0200

    Added fix for variable names that conflict with Jimple reserved words.

--- tools/gcc-bridge/src/main/java/org/renjin/gcc/jimple/Jimple.java
+++ tools/gcc-bridge/src/main/java/org/renjin/gcc/jimple/Jimple.java
@@ -1,11 +1,47 @@
 package org.renjin.gcc.jimple;
 
+import com.google.common.collect.Sets;
 import org.renjin.gcc.gimple.GimpleParameter;
 import org.renjin.gcc.gimple.expr.GimpleVariableRef;
 
+import java.util.Set;
+
 public class Jimple {
 
+  public static Set<String> RESERVED_WORDS = Sets.newHashSet(
+      "new",
+      "abstract",
+      "assert",
+      "boolean",
+      "break",
+      "byte",
+      "case",
+      "catch",
+      "char",
+      "class",
+      "const",
+      "continue",
+      "default",
+      "do",
+      "double",
+      "else",
+      "enum",
+      "extends",
+      "false",
+      "final",
+      "finally",
+      "float",
+      "for",
+      "goto",
+      "if",
+      "implements",
+      "import");
+
+
   public static String id(String name) {
+    if(RESERVED_WORDS.contains(name)) {
+      return name + "__";
+    }
     return name.replace('.', '$');
   }
 

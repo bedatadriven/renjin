870d2f00e2739b60fb350c56a6899509baf5103a
Alexander Bertram <alex@bedatadriven.com>
2013-07-19 22:31:16 +0200

    Added tests for environment name formatting

---
+++ tests/src/test/R/envNames.R
@@ -0,0 +1,7 @@
+
+
+test.searchPath <- function() {
+    assertThat(search(), equalTo(c(".GlobalEnv", "package:stats", "package:graphics",
+            "package:grDevices",  "package:utils", "package:datasets",
+            "package:methods", "Autoloads",  "package:base")))
+}

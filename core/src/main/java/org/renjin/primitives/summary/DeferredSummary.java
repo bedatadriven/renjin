9bc9fe904188b0215abb9e786827da4edef9639c
Alexander Bertram <alex@bedatadriven.com>
2013-11-02 08:08:34 -0400

    Merge pull request #36: Fixes bug in deferred sum

--- core/src/main/java/org/renjin/primitives/summary/DeferredSummary.java
+++ core/src/main/java/org/renjin/primitives/summary/DeferredSummary.java
@@ -50,7 +50,7 @@ public abstract class DeferredSummary extends DoubleVector implements MemoizedCo
   @Override
   public final Vector forceResult() {
     if(!calculated) {
-      calculate();
+      result = calculate();
     }
     return new DoubleArrayVector(result);
   }
@@ -59,5 +59,14 @@ public abstract class DeferredSummary extends DoubleVector implements MemoizedCo
   public final void setResult(Vector result) {
     this.result = result.getElementAsDouble(0);
     this.calculated = true;
+  }
+
+  @Override
+  public String toString() {
+    if(calculated) {
+      return Double.toString(result);
+    } else {
+      return "<deferred " + getComputationName() + ">";
+    }
   }
 }

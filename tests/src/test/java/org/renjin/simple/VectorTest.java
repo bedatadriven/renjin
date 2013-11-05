6f982433f060fc4e358464257a67d6a81deaad23
Alexander Bertram <alex@bedatadriven.com>
2013-11-05 02:31:30 +0100

    [Purdue Tests] Refactored more tests

--- tests/src/test/java/org/renjin/simple/VectorTest.java
+++ tests/src/test/java/org/renjin/simple/VectorTest.java
@@ -10,12 +10,12 @@ public class VectorTest extends SimpleTestBase {
     assertEval("{ x<-1:10; x[3L] }", "3L");
     assertEval("{ x<-c(1,2,3); x[3] }", "3.0");
     assertEval("{ x<-c(1,2,3); x[3L] }", "3.0");
-    assertEval("{ x<-1:3; x[0-2] }", "1L, 3L");
+    assertEval("{ x<-1:3; x[0-2] }", "c(1L, 3L)");
     assertEval("{ x<-1:3; x[FALSE] }", "integer(0)");
-    assertEval("{ x<-1:3; x[TRUE] }", "1L, 2L, 3L");
-    assertEval("{ x<-c(TRUE,TRUE,FALSE); x[0-2] }", "TRUE, FALSE");
+    assertEval("{ x<-1:3; x[TRUE] }", "c(1L, 2L, 3L)");
+    assertEval("{ x<-c(TRUE,TRUE,FALSE); x[0-2] }", "c(TRUE, FALSE)");
     assertEval("{ x<-c(1,2);x[[0-1]] }", "2.0");
-    assertEval("{ x<-c(1,2);x[0-3] }", "1.0, 2.0");
+    assertEval("{ x<-c(1,2);x[0-3] }", "c(1.0, 2.0)");
     assertEval("{ x<-10; x[0-1] }", "numeric(0)");
     assertEval("{ x<-10; x[NA] }", "NA");
 

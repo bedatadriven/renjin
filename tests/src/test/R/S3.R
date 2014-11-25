39ec987beb6b81e36a7b8b71eb7fcf9adbe1dd26
alex@bedatadriven.com <alex@bedatadriven.com>
2014-11-25 17:47:46 +0100

    Fixes dispatching of NextMethod() when .default method is not exported from package

--- tests/src/test/R/S3.R
+++ tests/src/test/R/S3.R
@@ -1,6 +1,10 @@
-
 test.factorSubset <- function() {
 
     f <- factor(c("Yes", "No"))
     f <- f[, drop=TRUE]
+}
+
+test.defaultMethodsInPackages <- function() {
+    df <- data.frame(x=1:10)
+    print(utils::str(df))
 }

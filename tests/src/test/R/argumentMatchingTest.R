f79973a54b2606f66a336b40e2f5495a033084f9
Alex Bertram <alex@bedatadriven.com>
2015-04-20 10:40:21 +0200

    Removed debuging code from argumentMatchingTest.R

--- tests/src/test/R/argumentMatchingTest.R
+++ tests/src/test/R/argumentMatchingTest.R
@@ -1,11 +1,12 @@
 
+library(hamcrest)
+
 test.noPartialMatchingWhenElipisesIsPresent <- function() {
 
     f <- function(... , aardvark) names(list(...))
     
     matchedArgs <- f(a = 41, b = 42, c = 43)
     
-    print(matchedArgs)
-    stopifnot(identical(matchedArgs, c('a', 'b', 'c')))
+    assertThat(matchedArgs, identicalTo(c('a', 'b', 'c')))
 
 }

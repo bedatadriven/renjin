bedatadriven/renjin

commit 266ebeec286f68c0d51f15a4fa2627aa54571981

replaced constant vectors with rep vectors of size 1

core/src/main/java/org/renjin/primitives/matrix/Matrices.java
@@ -8,11 +8,21 @@ import org.renjin.invoke.annotations.Internal;
 import org.renjin.primitives.Indexes;
 import org.renjin.primitives.Warning;
 import org.renjin.primitives.sequence.RepDoubleVector;
+import org.renjin.primitives.sequence.RepLogicalVector;
 import org.renjin.primitives.vector.ComputingIntVector;
-import org.renjin.primitives.vector.ConstantDoubleVector;
-import org.renjin.primitives.vector.ConstantLogicalVector;
 import org.renjin.primitives.vector.DeferredComputation;
-import org.renjin.sexp.*;
+import org.renjin.sexp.AtomicVector;
+import org.renjin.sexp.AttributeMap;
+import org.renjin.sexp.DoubleArrayVector;
+import org.renjin.sexp.DoubleVector;
+import org.renjin.sexp.IntArrayVector;
+import org.renjin.sexp.IntVector;
+import org.renjin.sexp.ListVector;
+import org.renjin.sexp.LogicalVector;
+import org.renjin.sexp.PairList;
+import org.renjin.sexp.SEXP;
+import org.renjin.sexp.Symbols;
+import org.renjin.sexp.Vector;
 
 
 /**
@@ -339,11 +349,7 @@ public class Matrices {
     int resultLength = (nrow * ncol);
     if(!byRow && resultLength > 500) {
       if(data instanceof DoubleVector) {
-        if(data.length() == 1) {
-          return new ConstantDoubleVector(data.getElementAsDouble(0), resultLength, attributes);
-        } else {
-          return new RepDoubleVector(data, resultLength, 1, attributes);
-        }
+        return new RepDoubleVector(data, resultLength, 1, attributes);
       }
     }
     return allocMatrix(data, nrow, ncol, byRow, dimnames);
@@ -356,7 +362,7 @@ public class Matrices {
     if (dataLength == 1 && data instanceof LogicalVector) {
       /* If data has only one entry, we can get away with a constant. 
        * This is true for the common case of matrix(nrow=42, ncol=42)  */
-      result = ConstantLogicalVector.newConstantBuilder(data.getElementAsRawLogical(0), nrow * ncol);
+      result = RepLogicalVector.newConstantBuilder(data.getElementAsLogical(0), nrow * ncol);
     } else {
       result = data.newBuilderWithInitialSize(nrow * ncol);
       if(dataLength > 0) {

core/src/main/java/org/renjin/compiler/pipeline/VectorPipeliner.java
@@ -5,7 +5,7 @@ import org.renjin.sexp.Vector;
 
 public interface VectorPipeliner {
 
-  public static boolean DEBUG = System.getProperty("renjin.vp.debug") != null;
+  public static boolean DEBUG = true; //System.getProperty("renjin.vp.debug") != null;
 
   public static int MAX_DEPTH = 25;
 

core/src/main/java/org/renjin/primitives/Vectors.java
@@ -1,21 +1,55 @@
 package org.renjin.primitives;
 
-import com.google.common.base.Charsets;
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
+import java.util.Arrays;
+
 import org.apache.commons.math.complex.Complex;
 import org.renjin.eval.Context;
 import org.renjin.eval.EvalException;
-import org.renjin.invoke.annotations.*;
-import org.renjin.invoke.reflection.converters.*;
-import org.renjin.primitives.vector.ConstantDoubleVector;
+import org.renjin.invoke.annotations.ArgumentList;
+import org.renjin.invoke.annotations.Builtin;
+import org.renjin.invoke.annotations.Current;
+import org.renjin.invoke.annotations.DataParallel;
+import org.renjin.invoke.annotations.Generic;
+import org.renjin.invoke.annotations.Internal;
+import org.renjin.invoke.annotations.Recycle;
+import org.renjin.invoke.reflection.converters.BooleanArrayConverter;
+import org.renjin.invoke.reflection.converters.BooleanConverter;
+import org.renjin.invoke.reflection.converters.DoubleArrayConverter;
+import org.renjin.invoke.reflection.converters.DoubleConverter;
+import org.renjin.invoke.reflection.converters.IntegerArrayConverter;
+import org.renjin.invoke.reflection.converters.IntegerConverter;
+import org.renjin.invoke.reflection.converters.StringArrayConverter;
+import org.renjin.invoke.reflection.converters.StringConverter;
+import org.renjin.primitives.sequence.RepDoubleVector;
 import org.renjin.primitives.vector.ConvertingDoubleVector;
 import org.renjin.primitives.vector.ConvertingStringVector;
 import org.renjin.primitives.vector.DeferredComputation;
-import org.renjin.sexp.*;
+import org.renjin.sexp.AtomicVector;
+import org.renjin.sexp.AttributeMap;
+import org.renjin.sexp.ComplexArrayVector;
+import org.renjin.sexp.DoubleArrayVector;
+import org.renjin.sexp.DoubleVector;
+import org.renjin.sexp.ExternalPtr;
+import org.renjin.sexp.IntArrayVector;
+import org.renjin.sexp.IntVector;
+import org.renjin.sexp.ListVector;
+import org.renjin.sexp.Logical;
+import org.renjin.sexp.LogicalArrayVector;
+import org.renjin.sexp.LogicalVector;
+import org.renjin.sexp.Null;
+import org.renjin.sexp.PairList;
+import org.renjin.sexp.RawVector;
+import org.renjin.sexp.SEXP;
+import org.renjin.sexp.StringArrayVector;
+import org.renjin.sexp.StringVector;
+import org.renjin.sexp.Symbol;
+import org.renjin.sexp.Symbols;
+import org.renjin.sexp.Vector;
 import org.renjin.util.NamesBuilder;
 
-import java.util.Arrays;
+import com.google.common.base.Charsets;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 
 /**
  * Functions which operate on Vectors
@@ -293,7 +327,7 @@ public class Vectors {
       return new IntArrayVector(new int[length]);
 
     } else if ("numeric".equals(mode) || "double".equals(mode)) {
-      return new ConstantDoubleVector(0, length);
+      return new RepDoubleVector(0, length);
 
     } else if ("complex".equals(mode)) {
       throw new UnsupportedOperationException("implement me!");

core/src/main/java/org/renjin/primitives/combine/LazyBuilder.java
@@ -1,17 +1,24 @@
 package org.renjin.primitives.combine;
 
 
-import com.google.common.base.Strings;
-import com.google.common.collect.Lists;
+import java.util.List;
+
 import org.renjin.primitives.combine.view.CombinedDoubleVector;
 import org.renjin.primitives.combine.view.CombinedIntVector;
 import org.renjin.primitives.combine.view.CombinedStringVector;
 import org.renjin.primitives.sequence.IntSequence;
-import org.renjin.primitives.vector.ConstantStringVector;
+import org.renjin.primitives.sequence.RepStringVector;
 import org.renjin.primitives.vector.PrefixedStringVector;
-import org.renjin.sexp.*;
+import org.renjin.sexp.AttributeMap;
+import org.renjin.sexp.DoubleVector;
+import org.renjin.sexp.IntVector;
+import org.renjin.sexp.Null;
+import org.renjin.sexp.SEXP;
+import org.renjin.sexp.StringVector;
+import org.renjin.sexp.Vector;
 
-import java.util.List;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 
 public class LazyBuilder implements CombinedBuilder {
 
@@ -62,7 +69,7 @@ public class LazyBuilder implements CombinedBuilder {
 
     if(Strings.isNullOrEmpty(prefix) && names == null) {
       // both argument name and names() vector are absent
-      return new ConstantStringVector("", numElements);
+      return new RepStringVector("", numElements);
 
     } else if(Strings.isNullOrEmpty(prefix)) {
       // argument name is missing, but we have names() vector

core/src/main/java/org/renjin/primitives/combine/view/CombinedIntVector.java
@@ -38,6 +38,10 @@ public class CombinedIntVector extends IntVector implements DeferredComputation 
 	public Vector[] getOperands() {
 		return vectors;
 	}
+	
+	public int[] getEndIndices() {
+	  return endIndex;
+	}
 
 	@Override
 	public String getComputationName() {

core/src/main/java/org/renjin/primitives/sequence/RepDoubleVector.java
@@ -2,7 +2,12 @@ package org.renjin.primitives.sequence;
 
 
 import org.renjin.primitives.vector.DeferredComputation;
-import org.renjin.sexp.*;
+import org.renjin.sexp.AttributeMap;
+import org.renjin.sexp.DoubleArrayVector;
+import org.renjin.sexp.DoubleVector;
+import org.renjin.sexp.IntArrayVector;
+import org.renjin.sexp.SEXP;
+import org.renjin.sexp.Vector;
 
 public class RepDoubleVector extends DoubleVector implements DeferredComputation {
 
@@ -12,6 +17,12 @@ public class RepDoubleVector extends DoubleVector implements DeferredComputation
   private int length;
   private int each;
 
+  public RepDoubleVector(double constant, int length) {
+    super(AttributeMap.EMPTY);
+    this.source = DoubleVector.valueOf(constant);
+    this.length = length;
+    this.each = 1;
+  }
   public RepDoubleVector(Vector source, int length, int each, AttributeMap attributes) {
     super(attributes);
     this.source = source;

core/src/main/java/org/renjin/primitives/sequence/RepLogicalVector.java
@@ -1,0 +1,113 @@
+package org.renjin.primitives.sequence;
+
+import org.renjin.eval.EvalException;
+import org.renjin.sexp.AttributeMap;
+import org.renjin.sexp.Logical;
+import org.renjin.sexp.LogicalVector;
+import org.renjin.sexp.SEXP;
+import org.renjin.sexp.Vector;
+
+public class RepLogicalVector extends LogicalVector {
+
+  public static final int LENGTH_THRESHOLD = 100;
+
+  private final Vector source;
+  private int length;
+  private int each;
+
+  public RepLogicalVector(Vector source, int length, int each, AttributeMap attributes) {
+    super(attributes);
+    this.source = source;
+    this.length = length;
+    this.each = each;
+    if(this.length <= 0) {
+      throw new IllegalArgumentException("length: " + length);
+    }
+  }
+
+  public RepLogicalVector(Vector source, int length, int each) {
+    this(source, length, each, transformAttributes(source, length, each));
+  }
+
+  private static AttributeMap transformAttributes(Vector source, int length, int each) {
+    if(source.getAttributes().hasNames()) {
+      return source.getAttributes()
+              .copy()
+              .setNames(new RepStringVector(source.getAttributes().getNames(), length, each, AttributeMap.EMPTY))
+              .build();
+    } else {
+      return source.getAttributes();
+    }
+  }
+
+  @Override
+  protected SEXP cloneWithNewAttributes(AttributeMap attributes) {
+    return new RepDoubleVector(source, length, each, attributes);
+  }
+
+  @Override
+  public boolean isConstantAccessTime() {
+    return true;
+  }
+
+  @Override
+  public int length() {
+    return length;
+  }
+
+  @Override
+  public int getElementAsRawLogical(int index) {
+    return source.getElementAsInt((index / each) % source.length());
+  }
+  
+  public static Builder newConstantBuilder(Logical value, int length) {
+    return new Builder(value, length);
+  }
+  
+  public static class Builder extends AbstractAtomicBuilder {
+      private LogicalVector vector;
+      private int length = 1;
+      private int each = 1;
+
+      public Builder(Logical value, int length) {
+        switch (value) {
+        case TRUE:
+          this.vector = LogicalVector.TRUE;
+          break;
+        case FALSE:
+          this.vector = LogicalVector.FALSE;
+          break;
+        default:
+          this.vector = LogicalVector.NA_VECTOR;
+          break;
+        }
+        this.length = length;
+      }
+      
+      @Override
+      public int length() {
+        return length;
+      }
+
+      @Override
+      public LogicalVector build() {
+        return new RepLogicalVector(vector, length, each, buildAttributes());
+      }
+
+      @Override
+      public Builder setNA(int index) {
+        throw new EvalException("cannot set na on constant builder");
+      }
+
+      @Override
+      public Builder setFrom(int destinationIndex,
+          Vector source, int sourceIndex) {
+        throw new EvalException("cannot set from on constant builder");
+      }
+
+      @Override
+      public Builder add(Number value) {
+       throw new EvalException("cannot add something to constant builder");
+      }
+    }
+}

core/src/main/java/org/renjin/primitives/sequence/RepStringVector.java
@@ -12,6 +12,14 @@ public class RepStringVector extends StringVector {
   private int length;
   private int each;
 
+  public RepStringVector(String constant, int length) {
+    super(AttributeMap.EMPTY);
+    this.source = StringVector.valueOf(constant);
+    this.sourceLength = source.length();
+    this.length = length;
+    this.each = 1;
+  }
+  
   public RepStringVector(Vector source, int length, int each, AttributeMap attributes) {
     super(attributes);
     this.source = source;

commit e62ae2cc6ad149a9c00b1199a3f7d5ccc45b8b18

Fixed regression in matrix() introduced by 266ebeec286f68c0d51f15a4fa2627aa54571981

core/src/main/java/org/renjin/primitives/matrix/Matrices.java
@@ -355,11 +355,12 @@ public class Matrices {
   private static Vector allocMatrix(Vector data, int nrow, int ncol, boolean byRow, Vector dimnames) {
 	  Vector.Builder result = null;
 	  int dataLength = data.length();
+      int outLength = nrow * ncol;
     
-    if (dataLength == 1 && data instanceof LogicalVector) {
+    if (dataLength == 1 && outLength > 0 && data instanceof LogicalVector) {
       /* If data has only one entry, we can get away with a constant. 
        * This is true for the common case of matrix(nrow=42, ncol=42)  */
-      result = RepLogicalVector.newConstantBuilder(data.getElementAsLogical(0), nrow * ncol);
+      result = RepLogicalVector.newConstantBuilder(data.getElementAsLogical(0), outLength);
     } else {
       result = data.newBuilderWithInitialSize(nrow * ncol);
       if(dataLength > 0) {

core/src/test/java/org/renjin/primitives/MatrixTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.renjin.EvalTestCase;
 import org.renjin.sexp.DoubleVector;
+import org.renjin.sexp.Logical;
 import org.renjin.sexp.Vector;
 import org.renjin.util.CommonsMath;
 
@@ -53,6 +54,13 @@ public class MatrixTest extends EvalTestCase {
     assertThat(eval("matrix(c(1,2,3,4),2,2,TRUE)"), equalTo(c(1, 3, 2, 4)));
     assertThat(eval("matrix(c(1,2,3,4),2,4,TRUE)"), equalTo(c(1, 1, 2, 2, 3, 3, 4, 4)));
     assertThat(eval("as.double(matrix(1:10,5,2,TRUE))"), equalTo(c(1,3,5,7,9,2,4,6,8,10)));
+  }
+  
+  @Test
+  public void matrixWithZeroLengthArg() {
+    eval("x <- matrix(nrow=0,ncol=0)");
+    assertThat(eval("dim(x)"), equalTo(c_i(0, 0)));
+    assertThat(eval("length(x)"), equalTo(c_i(0)));
   }
 
   @Test
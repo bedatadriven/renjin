bbc0cf9cada8b616815a925de14badfea47332ba
Alex Bertram <alex@bedatadriven.com>
2015-09-20 15:28:32 +0200

    Fixed mget test

--- tests/src/test/R/mget.R
+++ tests/src/test/R/mget.R
@@ -8,7 +8,7 @@ test.mget <- function() {
     env$b <- "BAR"
     env$c <- "ZOO"
     
-    result <- mget(c("a", "b", "z"), ifnotfound=42)
+    result <- mget(c("a", "b", "z"), env, ifnotfound=42)
     
     assertThat(result, identicalTo(list(a="FOO", b="BAR", z=42)))
 }

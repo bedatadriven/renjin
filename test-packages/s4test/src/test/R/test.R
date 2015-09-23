4e9f9a657f3c0402151c4acc0caf0840834288a5
Alex Bertram <alex@bedatadriven.com>
2015-09-23 15:31:56 +0200

    Fixed s4test package
    - Fixed pom.xml: if you need packages, even default ones like "methods" or "stats",
       they need to be declared as dependencies here
     - Corrected typeos in tests

--- test-packages/s4test/src/test/R/test.R
+++ test-packages/s4test/src/test/R/test.R
@@ -1,11 +1,13 @@
+library(stats4)
 library(s4test)
 library(hamcrest)
 
+
 test.seqType.ClassExport.ToNamespace = function(){
 
-    d = new("Gene", sequence = "ACCATCG", name = "Kozak", type = "")
-    r = new("Gene", sequence = "AUG", name = "Startcodon", type = "")
-    p = new("Gene", sequence = "VNAGNVQELHIG", name = "ProtTest", type = "")
+    d = new("seq", sequence = "ACCATCG", name = "Kozak", type = "")
+    r = new("seq", sequence = "AUG", name = "Startcodon", type = "")
+    p = new("seq", sequence = "VNAGNVQELHIG", name = "ProtTest", type = "")
 
     d = setType(d)
     r = setType(r)
@@ -26,15 +28,15 @@ test.seqType.ClassExport.ToNamespace = function(){
         identicalTo("protein"))
 }
 
-test.ExtendClassFromMethodAndExportToNamespace = function(){
+test.ExtendClassFromMethodAndExportToNamespace = function() {
 
-a = new("mle")
-b = new("mle_ext")
-
-assertThat( class(b@input_file), identicalTo("character") )
-
-assertThat( {class(a@input_file)}, ThrowsError )
-
-assertThat( {class(a@coef)}, identicalTo("numeric") )
+    a = new("mle")
+    b = new("mle_ext")
+    
+    assertThat( class(b@input_file), identicalTo("character") )
+    
+    assertThat( class(a@input_file), throwsError() )
+    
+    assertThat( class(a@coef), identicalTo("numeric") )
 
 }

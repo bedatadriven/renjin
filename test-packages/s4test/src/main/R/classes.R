6e51bc793c8b78b55bffcc97822087fcb412987b
psolaimani <p.solaimani@gmail.com>
2015-09-24 11:30:50 +0200

    corrected error in testcases and setMethods

--- test-packages/s4test/src/main/R/classes.R
+++ test-packages/s4test/src/main/R/classes.R
@@ -1,151 +1,128 @@
 setClass("seq",
-    slots = c(
-        name = "character",
-        sequence = "character",
-        type = "character"
-        ),
-    prototype = c(
-        name = "SequenceName",
-        sequence = "A",
-        type = ""
-    )
+         slots = c(
+           name = "character",
+           sequence = "character",
+           type = "character"
+         ),
+         prototype = c(
+           name = "SequenceName",
+           sequence = "A",
+           type = ""
+         )
 )
 
 setGeneric(name = "setType",
-    def = function(object,type){
-        standardGeneric("setType")
-    }
+           def = function(object,type){
+             standardGeneric("setType")
+           }
 )
 
 setMethod(f = "setType",
-    definition = function(object,type){
-        object@type <- type
-        return(object)
-    }
+          definition = function(object,type){
+            object@type <- type
+            return(object)
+          }
 )
 
 setGeneric(name = "getType",
-    def = function(object,type){
-        standardGeneric("getType")
-    }
+           def = function(object,type){
+             standardGeneric("getType")
+           }
 )
 setMethod(f = "getType",
-    definition = function(object,type){
-        return(object@type)
-    }
+          definition = function(object,type){
+            return(object@type)
+          }
 )
 
 
 setGeneric(name = "setName",
-    def = function(object,name){
-        standardGeneric("setName")
-    }
+           def = function(object,name){
+             standardGeneric("setName")
+           }
 )
 setMethod(f = "setName",
-    definition = function(object,name){
-        object@name <- name
-        return(object)
-    }
+          definition = function(object,name){
+            object@name <- name
+            return(object)
+          }
 )
 
 setGeneric(name = "getName",
-    def = function(object,name){
-        standardGeneric("getName")
-    }
+           def = function(object,name){
+             standardGeneric("getName")
+           }
 )
 setMethod(f = "getName",
-    definition = function(object,name){
-        return(object@name)
-    }
+          definition = function(object,name){
+            return(object@name)
+          }
 )
 
 
 setGeneric(name = "setSequence",
-    def = function(object,sequence){
-        standardGeneric("setSequence")
-    }
+           def = function(object,sequence){
+             standardGeneric("setSequence")
+           }
 )
 setMethod(f = "setSequence",
-    definition = function(object,sequence){
-        object@sequence <- sequence
-        return(object)
-    }
+          definition = function(object,sequence){
+            object@sequence <- sequence
+            return(object)
+          }
 )
 
 setGeneric(name = "getSequence",
-    def = function(object,sequence){
-        standardGeneric("getSequence")
-    }
+           def = function(object,sequence){
+             standardGeneric("getSequence")
+           }
 )
 setMethod(f = "getSequence",
-    definition = function(object,sequence){
-        return(object@sequence)
-    }
+          definition = function(object,sequence){
+            return(object@sequence)
+          }
 )
 
-setGeneric(name = "seqType",
-    def = function(seq){
-        standardGeneric("seqType")
-    }
+setGeneric(name = "findType",
+           def = function(seq){
+             standardGeneric("findType")
+           }
 )
 
+setMethod(f = "findType",
+          signature = "seq",
+          definition = function(seq){
+            DNA = c("A","T","C","G")
+            RNA = c("A","U","C","G")
+            PROTEIN = c("A","R","N","D","C",
+                        "E","Q","G","H","I",
+                        "L","K","M","F","P",
+                        "S","T","W","Y","V")
 
-setMethod(f = "seqType",
-    signature = "seq",
-    definition = function(seq){
-        DNA = c("A","T","C","G")
-        RNA = c("A","U","C","G")
-        PROTEIN = c("A","R","N","D","C",
-                    "E","Q","G","H","I",
-                    "L","K","M","F","P",
-                    "S","T","W","Y","V")
+            notDNA = is.element("FALSE",is.element(strsplit(getSequence(seq),split="")[[1]],DNA))
+            notRNA = is.element("FALSE",is.element(strsplit(getSequence(seq),split="")[[1]],RNA))
+            notPROTEIN = is.element("FALSE",is.element(strsplit(getSequence(seq),split="")[[1]],PROTEIN))
 
-        notDNA = is.element("FALSE",is.element(strsplit(getSequence(seq),split="")[[1]],DNA))
-        notRNA = is.element("FALSE",is.element(strsplit(getSequence(seq),split="")[[1]],RNA))
-        notPROTEIN = is.element("FALSE",is.element(strsplit(getSequence(seq),split="")[[1]],PROTEIN))
-
-        if(notDNA == FALSE & notRNA == TRUE & notPROTEIN == FALSE){
-            #cat("notDNA: ",notDNA,"\n")
-            #cat("notRNA: ",notRNA,"\n")
-            #cat("notPROTEIN: ",notPROTEIN,"\n")
-            #cat("Set as: DNA\n")
-            return(setType(seq,"DNA"))
-        } else if(notDNA == TRUE & notRNA == FALSE & notPROTEIN == TRUE){
-            #cat("notDNA: ",notDNA,"\n")
-            #cat("notRNA: ",notRNA,"\n")
-            #cat("notPROTEIN: ",notPROTEIN,"\n")
-            #cat("Set as: RNA\n")
-            return(setType(seq,"RNA"))
-        } else if(notDNA == TRUE & notRNA == TRUE & notPROTEIN == FALSE){
-            #cat("notDNA: ",notDNA,"\n")
-            #cat("notRNA: ",notRNA,"\n")
-            #cat("notPROTEIN: ",notPROTEIN,"\n")
-            #cat("Set as: protein\n")
-            return(setType(seq,"protein"))
-        } else if(notDNA == FALSE & notRNA == FALSE & notPROTEIN == TRUE){
-            #cat("Could not distinquish between DNA or RNA, no U/T present in sequence. Type set as DNA\n")
-            #cat("notDNA: ",notDNA,"\n")
-            #cat("notRNA: ",notRNA,"\n")
-            #cat("notPROTEIN: ",notPROTEIN,"\n")
-            #cat("Set as: DNA\n")
-            return(setType(seq,"DNA"))
-        } else {
-            #cat("The type of input sequence could not be determined and is defined as unknown\n")
-            #cat("notDNA: ",notDNA,"\n")
-            #cat("notRNA: ",notRNA,"\n")
-            #cat("notPROTEIN: ",notPROTEIN,"\n")
-            #cat("Set as: unknown\n")
-            return(setType(seq,"unknown"))
-        }
-    }
+            if(notDNA == FALSE & notRNA == TRUE & notPROTEIN == FALSE){
+              return(setType(seq,"DNA"))
+            } else if(notDNA == TRUE & notRNA == FALSE & notPROTEIN == TRUE){
+              return(setType(seq,"RNA"))
+            } else if(notDNA == TRUE & notRNA == TRUE & notPROTEIN == FALSE){
+              return(setType(seq,"protein"))
+            } else if(notDNA == FALSE & notRNA == FALSE & notPROTEIN == TRUE){
+              return(setType(seq,"DNA"))
+            } else {
+              return(setType(seq,"unknown"))
+            }
+          }
 )
 
 mle_ext = setClass("mle_ext",
-    slots = c(
-        input_file = "character"
-    ),
-    prototype = c(
-        input_file = "INPUT_OBJECT"
-    ),
-    contains = "mle"
+                   slots = c(
+                     input_file = "character"
+                   ),
+                   prototype = c(
+                     input_file = "INPUT_OBJECT"
+                   ),
+                   contains = "mle"
 )
--- test-packages/s4test/src/test/R/test.R
+++ test-packages/s4test/src/test/R/test.R
@@ -4,39 +4,54 @@ library(hamcrest)
 
 
 test.seqType.ClassExport.ToNamespace = function(){
-
-    d = new("seq", sequence = "ACCATCG", name = "Kozak", type = "")
-    r = new("seq", sequence = "AUG", name = "Startcodon", type = "")
-    p = new("seq", sequence = "VNAGNVQELHIG", name = "ProtTest", type = "")
-
-    d = setType(d)
-    r = setType(r)
-    p = setType(p)
-
-    assertThat(
-        d@type,
-        identicalTo("DNA")
-        )
-
-    assertThat(
-        r@type,
-        identicalTo("RNA")
-        )
-
-    assertThat(
-        p@type,
-        identicalTo("protein"))
+  
+  d = new("seq", sequence = "ACCATCG", name = "Kozak", type = "")
+  r = new("seq", sequence = "AUG", name = "Startcodon", type = "")
+  p = new("seq", sequence = "VNAGNVQELHIG", name = "ProtTest", type = "")
+  
+  d = findType(d)
+  r = findType(r)
+  p = findType(p)
+  
+  assertThat(
+    d2@type,
+    identicalTo("DNA")
+  )
+  
+  assertThat(
+    r2@type,
+    identicalTo("RNA")
+  )
+  
+  assertThat(
+    p2@type,
+    identicalTo("protein")
+    )
 }
 
 test.ExtendClassFromMethodAndExportToNamespace = function() {
-
-    a = new("mle")
-    b = new("mle_ext")
-    
-    assertThat( class(b@input_file), identicalTo("character") )
-    
-    assertThat( class(a@input_file), throwsError() )
-    
-    assertThat( class(a@coef), identicalTo("numeric") )
-
+  
+  a = new("mle")
+  b = new("mle_ext")
+  
+  assertThat( class(b@input_file), identicalTo("character") )
+  
+  assertThat( class(a@input_file), throwsError() )
+  
+  assertThat( class(a@coef), identicalTo("numeric") )
+  
+  assertThat( class(a@coef), identicalTo(class(b@coef)) )
+  
+  assertThat( show(a), throwsError() )
+  
+  
+  setClass("new_ext_mle",
+           prototype = c(B = 100, c= "character"),
+           contains = "mle_ext"
+           )
+  
+  e = new("new_ext_mle")
+  
+  assertThat( e@names, identicalTo(c("B","c")))
+  
 }

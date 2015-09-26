7aae085d2dc88f70d9de2aebc34516af4c204958
Alex Bertram <alex@bedatadriven.com>
2015-09-26 14:20:09 +0200

    Cleaned up namespace lookup / loading logic

--- core/src/main/java/org/renjin/primitives/packaging/NamespaceRegistry.java
+++ core/src/main/java/org/renjin/primitives/packaging/NamespaceRegistry.java
@@ -3,10 +3,7 @@ package org.renjin.primitives.packaging;
 import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Multimap;
-import com.google.common.collect.Sets;
+import com.google.common.collect.*;
 import com.google.common.io.CharSource;
 import org.renjin.eval.Context;
 import org.renjin.eval.EvalException;
@@ -14,7 +11,6 @@ import org.renjin.invoke.annotations.SessionScoped;
 import org.renjin.sexp.*;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -33,7 +29,7 @@ public class NamespaceRegistry {
    * org.renjin groupId.
    */
   private static final Set<String> CORE_PACKAGES = Sets.newHashSet("datasets", "graphics", "grDevices", "hamcrest",
-          "methods", "splines", "stats", "stats4", "utils", "grid", "parallel", "tools");
+      "methods", "splines", "stats", "stats4", "utils", "grid", "parallel", "tools");
 
   private PackageLoader loader;
 
@@ -86,74 +82,51 @@ public class NamespaceRegistry {
     if(symbol.getPrintName().equals("base")) {
       return baseNamespace;
     }
-    
+
     // try to match name to currently loaded namespaces
     for (FqPackageName fqPackageName : namespaceMap.keySet()) {
       if(symbol.getPrintName().equals(fqPackageName.toString('.')) ||
-         symbol.getPrintName().equals(fqPackageName.getPackageName())) {
+          symbol.getPrintName().equals(fqPackageName.getPackageName())) {
         return namespaceMap.get(fqPackageName);
       }
     }
-    
-      
+
+    // There are a small number of "core" packages that are part of the 
+    // the GNU R source, and now the Renjin source. (e.g., stats, methods, datasets, etc)
+    // These have the groupId "org.renjin"
+    if(CORE_PACKAGES.contains(symbol.getPrintName())) {
+      return getNamespace(FqPackageName.corePackage(symbol));
+    }
+
+    // Otherwise, try to guess the groupId
+    List<FqPackageName> candidates = Lists.newArrayList();
+
     if(couldBeFullyQualified(symbol)) {
+      candidates.add(FqPackageName.fromSymbol(symbol));
+    }
 
-      // assume we've been provided with a nice, fully-qualified
-      // org.myGroup.myPackageName
+    // Try bioconductor first as some packages have moved from cran to bioconductor
+    candidates.add(new FqPackageName("org.renjin.bioconductor", symbol.getPrintName()));
+    candidates.add(new FqPackageName("org.renjin.cran", symbol.getPrintName()));
 
-      FqPackageName packageName = FqPackageName.fromSymbol(symbol);
-      Optional<Namespace> namespace = tryGetNamespace(packageName);
+    Optional<Namespace> namespace = Optional.absent();
+
+    for (FqPackageName candidate : candidates) {
+      namespace = tryGetNamespace(candidate);
       if(namespace.isPresent()) {
-        return namespace.get();
-      }
-
-      // this could be a cran package with a dot
-      namespace = tryGetNamespace(FqPackageName.cranPackage(symbol));
-      if(namespace.isPresent()) {
-        return namespace.get();
-      }
-
-      throw new EvalException("Could not find package '" + symbol + "'; tried both " +
-              packageName + " and " + FqPackageName.cranPackage(symbol));
-
-
-    } else {
-
-      // we've only got a local package name: use defaults for
-      // core package and then fall back to cran namespace
-
-      if(CORE_PACKAGES.contains(symbol.getPrintName())) {
-        return getNamespace(FqPackageName.corePackage(symbol));
-
-      } else {
-
-        // Try bioconductor first as some packages have moved from cran to bioconductor
-        List<FqPackageName> candidates = Arrays.asList(
-            new FqPackageName("org.renjin.bioconductor", symbol.getPrintName()),
-            new FqPackageName("org.renjin.cran", symbol.getPrintName()));
-        
-        
-        Optional<Namespace> namespace = Optional.absent();
-
-        for (FqPackageName candidate : candidates) {
-          namespace = tryGetNamespace(FqPackageName.cranPackage(symbol.getPrintName()));
-          if(namespace.isPresent()) {
-            break;
-          }
-        }
-        
-        if(!namespace.isPresent()) {
-          throw new EvalException("Could not load package " + symbol + "; tried " +
-              Joiner.on(", ").join(candidates));
-        }
-        
-        return namespace.get();
+        break;
       }
     }
+
+    if(!namespace.isPresent()) {
+      throw new EvalException("Could not load package " + symbol + "; tried " +
+          Joiner.on(", ").join(candidates));
+    }
+
+    return namespace.get();
   }
 
-  public static Set<String>  getCorePackages()
-  {
+  public static Set<String>  getCorePackages() {
     return CORE_PACKAGES;
   }
 
@@ -201,7 +174,7 @@ public class NamespaceRegistry {
         if(namespace.getNamespaceEnvironment().hasVariable(Symbol.get(".onLoad"))) {
           StringVector nameArgument = StringVector.valueOf(pkg.getName().getPackageName());
           context.evaluate(FunctionCall.newCall(Symbol.get(".onLoad"), nameArgument, nameArgument),
-                  namespace.getNamespaceEnvironment());
+              namespace.getNamespaceEnvironment());
         }
 
         return Optional.of(namespace);
@@ -236,7 +209,7 @@ public class NamespaceRegistry {
 
     CharSource namespaceSource = pkg.getResource("NAMESPACE").asCharSource(Charsets.UTF_8);
     NamespaceFile namespaceFile = new NamespaceFile(namespaceSource);
-    
+
     namespace.initImports(this, namespaceFile);
     namespace.initExports(namespaceFile);
     namespace.registerS3Methods(context, namespaceFile);
@@ -260,7 +233,7 @@ public class NamespaceRegistry {
     // BASE-NS -> IMPORTS -> ENVIRONMENT
 
     Environment imports = Environment.createNamedEnvironment(getBaseNamespaceEnv(),
-            "imports:" + pkg.getName().toString('.'));
+        "imports:" + pkg.getName().toString('.'));
 
     Environment namespaceEnv = Environment.createNamespaceEnvironment(imports, pkg.getName().getPackageName());
     Namespace namespace = new Namespace(pkg, namespaceEnv);

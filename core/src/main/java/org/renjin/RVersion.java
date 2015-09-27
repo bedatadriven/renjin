9c6823aa94e6e76a7799daceddf58e347b6e9d55
Alex Bertram <alex@bedatadriven.com>
2015-09-27 17:39:44 +0200

    Updated R version number to 3.2.0

--- core/src/main/java/org/renjin/RVersion.java
+++ core/src/main/java/org/renjin/RVersion.java
@@ -2,10 +2,10 @@ package org.renjin;
 
 public final class RVersion {
 
-  public static int MAJOR = 2;
-  public static int MINOR_1 = 14;
-  public static int MINOR_2 = 2;
+  public static int MAJOR = 3;
+  public static int MINOR_1 = 2;
+  public static int MINOR_2 = 0;
   public static String MINOR = (MINOR_1 + "." + MINOR_2);
   
-  public static String STRING = "2.14.2";
+  public static String STRING = "3.2.0";
 }

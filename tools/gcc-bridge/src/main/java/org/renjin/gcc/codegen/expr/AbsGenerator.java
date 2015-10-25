8516278c2c5bc846fc54a9a5083e82c90ba5fe04
Alex Bertram <alex@bedatadriven.com>
2015-10-25 19:30:30 +0100

    Corrected indent of AbsGenerator

--- tools/gcc-bridge/src/main/java/org/renjin/gcc/codegen/expr/AbsGenerator.java
+++ tools/gcc-bridge/src/main/java/org/renjin/gcc/codegen/expr/AbsGenerator.java
@@ -8,37 +8,38 @@ import org.renjin.gcc.gimple.type.GimpleType;
 
 public class AbsGenerator extends  AbstractExprGenerator implements ValueGenerator {
 
-    private ValueGenerator x;
+  private ValueGenerator x;
 
-    public AbsGenerator(ExprGenerator x) {
-        this.x = (ValueGenerator) x;
+  public AbsGenerator(ExprGenerator x) {
+    this.x = (ValueGenerator) x;
+  }
+
+  @Override
+  public GimpleType getGimpleType() {
+    return x.getGimpleType();
+  }
+
+
+  @Override
+  public void emitPrimitiveValue(MethodVisitor mv) {
+
+    x.emitPrimitiveValue(mv);
+
+    if(x.getJvmPrimitiveType().equals(Type.INT_TYPE)) {
+      mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Math", "abs", "(I)I", false);
+
+    } else if(x.getJvmPrimitiveType().equals(Type.DOUBLE_TYPE)) {
+      mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Math", "abs", "(D)D", false);
+
+    } else if(x.getJvmPrimitiveType().equals(Type.FLOAT_TYPE)) {
+      mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Math", "abs", "(F)F", false);
+
+    } else if(x.getJvmPrimitiveType().equals(Type.LONG_TYPE)) {
+      mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Math", "abs", "(J)J", false);
+
+    } else {
+      throw new UnsupportedOperationException(String.format("abs (%s)", x.getJvmPrimitiveType()));
     }
+  }
 
-    @Override
-    public GimpleType getGimpleType() {
-        return x.getGimpleType();
-    }
-
-    
-    @Override
-    public void emitPrimitiveValue(MethodVisitor mv) {
-
-        x.emitPrimitiveValue(mv);
-
-        if(x.getJvmPrimitiveType().equals(Type.INT_TYPE)) {
-            mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Math", "abs", "(I)I", false);
-
-        } else if(x.getJvmPrimitiveType().equals(Type.DOUBLE_TYPE)) {
-            mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Math", "abs", "(D)D", false);
-
-        } else if(x.getJvmPrimitiveType().equals(Type.FLOAT_TYPE)) {
-            mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Math", "abs", "(F)F", false);
-
-        } else if(x.getJvmPrimitiveType().equals(Type.LONG_TYPE)) {
-            mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Math", "abs", "(J)J", false);
-
-        } else {
-            throw new UnsupportedOperationException(String.format("abs (%s)", x.getJvmPrimitiveType()));
-        }
-    }
 }

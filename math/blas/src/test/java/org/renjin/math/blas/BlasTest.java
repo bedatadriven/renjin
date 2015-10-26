0b9161ff41e60cf13269822f45236693ea1ca661
Alex Bertram <alex@bedatadriven.com>
2015-10-26 17:35:01 +0100

    Imported blas 3.5.0 sources.
    Currently only serves as test for GCC-Bridge and a way to validate performance
    of generated code compared to f2j.

---
+++ math/blas/src/test/java/org/renjin/math/blas/BlasTest.java
@@ -0,0 +1,45 @@
+package org.renjin.math.blas;
+
+import org.junit.Test;
+import org.renjin.gcc.runtime.BytePtr;
+import org.renjin.gcc.runtime.DoublePtr;
+import org.renjin.gcc.runtime.IntPtr;
+
+/**
+ * Verifies that the compiled BLAS methods actually run
+ */
+public class BlasTest {
+
+  @Test
+  public void testDcabs() {
+    org.renjin.math.Blas.dcabs1_(new double[4], 0);
+  }
+
+  @Test
+  public void testDgemm() {
+    int m = 2;
+    double a[] = new double[] { 1, 2, 3, 4};
+    double b[] = new double[] { 5, 6, 7, 8};
+    double c[] = new double[m*m];
+    
+    IntPtr mPtr = new IntPtr(m);
+    // TRANSA,TRANSB,M,N,K,ALPHA,A,LDA,B,LDB,BETA,C,LDC)
+    org.renjin.math.Blas.dgemm_(
+        /* TRANSA */ BytePtr.asciiString("N"), 
+        /* TRANSB */ BytePtr.asciiString("N"),
+        /* M */ mPtr,
+        /* N */ mPtr,
+        /* K */ mPtr,
+        /* ALPHA */ new DoublePtr(1),
+        /* A */ new DoublePtr(a),
+        /* LDA */ mPtr,
+        /* B */ new DoublePtr(b),
+        /* LDB */ mPtr,
+        /* BETA */ new DoublePtr(0),
+        /* C */ new DoublePtr(c),
+        /* LDC */ mPtr,
+        /* LEN(TRANSA) */ 1,
+        /* LEN(TRANSB) */ 1 );
+
+  }
+}

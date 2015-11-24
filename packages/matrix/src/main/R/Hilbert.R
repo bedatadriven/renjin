0f5f8bc1d7d8c32c56c04b09a979a10de269170e
Alex Bertram <alex@bedatadriven.com>
2015-11-24 10:53:51 +0100

    Moved matrix package into seperate repo
    See https://github.com/bedatadriven/renjin-matrix

--- packages/matrix/src/main/R/Hilbert.R
+++
@@ -1,6 +0,0 @@
-Hilbert <- function(n)
-{   ## generate the Hilbert matrix of dimension n
-    n <- as.integer(n)
-    i <- seq_len(n)
-    new("dpoMatrix", x = c(1/outer(i - 1L, i, "+")), Dim = c(n,n))
-}
--- packages/matrix/src/main/c/lgCMatrix.h
+++
@@ -1,9 +0,0 @@
-#ifndef MATRIX_LGCMATRIX_H
-#define MATRIX_LGCMATRIX_H
-
-#include "Mutils.h"
-
-SEXP lgC_to_matrix(SEXP x);
-SEXP ngC_to_matrix(SEXP x);
-
-#endif

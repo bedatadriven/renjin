6dc20ae60d709ecdbb0d301d21117a2728779e09
psolaimani <p.solaimani@gmail.com>
2016-01-31 23:44:48 +0100

    add test cases for access as.dist() output attributes with attr()

--- tests/src/test/R/attributes.R
+++ tests/src/test/R/attributes.R
@@ -1,7 +1,7 @@
 
 library(hamcrest)
 
-test.AccessAttributeRegexpResult <- function() {
+test.AccessAttributeRegexpOutput <- function() {
 	a <- c("abc")
   b <- regexpr('(b)', a, perl = TRUE)
 
@@ -10,4 +10,18 @@ test.AccessAttributeRegexpResult <- function() {
 	assertThat( attr(b, "capture.length"), equalTo(1L) )
 	assertThat( attr(b, "match.length"), equalTo(1L) )
 	assertThat( attr(b, "useBytes"), equalTo(TRUE) )
+}
+
+test.AccessAttributeAsDistOutput <- function() {
+	a <- matrix(1:9,nrow=3)
+  row.names(a) <- c("A","B","C")
+  colnames(a) <- c("d","e","f")
+  d <- as.dist(a)
+
+	assertThat( attr(d, "Labels"), equalTo(c("A","B","C")) )
+	assertThat( attr(d, "Size"), equalTo(3L) )
+	assertThat( attr(d, "call"), equalTo("as.dist.default(m = a)") )
+	assertThat( attr(d, "class"), equalTo("dist") )
+	assertThat( attr(d, "Diag"), equalTo(FALSE) )
+	assertThat( attr(d, "Upper"), equalTo(FALSE) )
 }

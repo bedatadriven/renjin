ca4fb500e37d66cc72a9e407a78af30be85b8532
psolaimani <p.solaimani@gmail.com>
2016-02-01 00:12:10 +0100

    test cases for access attributes data.frames, matrices, lists

--- tests/src/test/R/attributes.R
+++ tests/src/test/R/attributes.R
@@ -1,6 +1,31 @@
 
 library(hamcrest)
 
+test.AccessAttributeDataframe <- function() {
+  a <- data.frame(a=c(1,2),b=c(2,1))
+  row.names(a) = c('A','B')
+
+	assertThat( attr(a, "names"), equalTo(c("a","b")) )
+	assertThat( attr(a, "row.names"), equalTo(c("A","B")) )
+	assertThat( attr(a, "class"), equalTo("data.frame") )
+
+}
+
+test.AccessAttributeMatrix <- function() {
+	a <- matrix(1:9,nrow=3)
+  row.names(a) <- c("A","B","C")
+  colnames(a) <- c("d","e","f")
+
+	assertThat( attr(a, "dim"), equalTo(c(3,3)) )
+	assertThat( attr(a, "dimnames")[[1]], equalTo(c("A","B","C")) )
+	assertThat( attr(a, "dimnames")[[2]], equalTo("d","e","f") )
+}
+
+test.AccessAttributeList <- function() {
+	a <- list(a=c(0), b = list())
+	assertThat( attr(a, "names"), equalTo(c("a","b")) )
+}
+
 test.AccessAttributeRegexpOutput <- function() {
 	a <- c("abc")
   b <- regexpr('(b)', a, perl = TRUE)

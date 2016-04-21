9d33c88ebe34e93290fc037c125ee9336bce005f
Parham Solaimani <parham@bedatadriven.com>
2016-04-21 11:44:56 +0200

    fix assignment to list elements using recursive indexing (fix issue #156)

--- tests/src/test/java/org/renjin/MicroTest.java
+++ tests/src/test/java/org/renjin/MicroTest.java
@@ -8335,6 +8335,10 @@ public class MicroTest extends AbstractMicroTest {
     assertIdentical("{ l <- list(c=list(d=3,e=c(f=4)), b=2, a=3) ; l[[c(\"c\")]] }", "structure(list(d = 3, e = structure(4, .Names = \"f\")), .Names = c(\"d\", \"e\"))");
   }
   @Test
+  public void micro2650() {
+    assertIdentical("{ f <- function(b, i, v) { b[[i]] <- v ; b } ; f(1:3,2,2) ; f(1:3,\"X\",2) ; f(list(1,list(2)),c(2,1),4) }", "list(1, list(4))");
+  }
+  @Test
   public void micro2651() {
     assertIdentical("{ l<-list(1,2L,TRUE) ; l[[2]]<-100 ; l }", "list(1, 100, TRUE)");
   }
@@ -8519,6 +8523,14 @@ public class MicroTest extends AbstractMicroTest {
     assertIdentical("{ l <- list(a=1,b=2,c=3) ; l[[\"b\"]] <- NULL ; l }", "structure(list(a = 1, c = 3), .Names = c(\"a\", \"c\"))");
   }
   @Test
+  public void micro2710() {
+    assertIdentical("{ l <- list(1,list(2,c(3))) ; l[[c(2,2)]] <- NULL ; l }", "list(1, list(2))");
+  }
+  @Test
+  public void micro2711() {
+    assertIdentical("{ l <- list(1,list(2,c(3))) ; l[[c(2,2)]] <- 4 ; l }", "list(1, list(2, 4))");
+  }
+  @Test
   public void micro2712() {
     assertIdentical("{ l <- list(1,list(2,list(3))) ; l[[1]] <- NULL ; l }", "list(list(2, list(3)))");
   }
@@ -8527,8 +8539,24 @@ public class MicroTest extends AbstractMicroTest {
     assertIdentical("{ l <- list(1,list(2,list(3))) ; l[[1]] <- 5 ; l }", "list(5, list(2, list(3)))");
   }
   @Test
+  public void micro2714() {
+    assertIdentical("{ l<-list(a=1,b=2,list(c=3,d=4,list(e=5:6,f=100))) ; l[[c(3,3,1)]] <- NULL ; l }", "structure(list(a = 1, b = 2, structure(list(c = 3, d = 4, structure(list(    f = 100), .Names = \"f\")), .Names = c(\"c\", \"d\", \"\"))), .Names = c(\"a\", \"b\", \"\"))");
+  }
+  @Test
+  public void micro2717() {
+    assertIdentical("{ l<-list(a=1,b=2,c=list(d=1,e=2,f=c(x=1,y=2,z=3))) ; l[[c(\"c\",\"f\")]] <- NULL ; l }", "structure(list(a = 1, b = 2, c = structure(list(d = 1, e = 2), .Names = c(\"d\", \"e\"))), .Names = c(\"a\", \"b\", \"c\"))");
+  }
+  @Test
   public void micro2718() {
     assertIdentical("{ l<-list(a=1,b=2,c=3) ; l[c(\"a\",\"a\",\"a\",\"c\")] <- NULL ; l }", "structure(list(b = 2), .Names = \"b\")");
+  }
+  @Test
+  public void micro2723() {
+    assertIdentical("{ l<-list(a=1L,b=2L,c=list(d=1L,e=2L,f=c(x=1L,y=2L,z=3L))) ; l[[c(\"c\",\"f\")]] <- 100L ; l }", "structure(list(a = 1L, b = 2L, c = structure(list(d = 1L, e = 2L,     f = 100L), .Names = c(\"d\", \"e\", \"f\"))), .Names = c(\"a\", \"b\", \"c\"))");
+  }
+  @Test
+  public void micro2724() {
+    assertIdentical("{ l<-list(a=1L,b=2L,c=list(d=1L,e=2L,f=c(x=1L,y=2L,z=3L))) ; l[[c(\"c\",\"f\")]] <- list(haha=\"gaga\") ; l }", "structure(list(a = 1L, b = 2L, c = structure(list(d = 1L, e = 2L,     f = structure(list(haha = \"gaga\"), .Names = \"haha\")), .Names = c(\"d\", \"e\", \"f\"))), .Names = c(\"a\", \"b\", \"c\"))");
   }
   @Test
   public void micro2725() {
@@ -8537,6 +8565,14 @@ public class MicroTest extends AbstractMicroTest {
   @Test
   public void micro2726() {
     assertIdentical("{ l<-list() ; x <- 1:3 ; l[[1]] <- x; x[2] <- 100L; l[[1]] }", "1:3");
+  }
+  @Test
+  public void micro2727() {
+    assertIdentical("{ l <- list(1, list(2)) ;  m <- l ; l[[c(2,1)]] <- 3 ; m[[2]][[1]] }", "2");
+  }
+  @Test
+  public void micro2728() {
+    assertIdentical("{ l <- list(1, list(2,3,4)) ;  m <- l ; l[[c(2,1)]] <- 3 ; m[[2]][[1]] }", "2");
   }
   @Test
   public void micro2729() {
@@ -8559,8 +8595,56 @@ public class MicroTest extends AbstractMicroTest {
     assertIdentical("{ f <- function() { l[1:2] <- x ; x[1] <- 211L  ; l[1] } ; l <- 1:3 ; x <- 10L ; f() }", "10L");
   }
   @Test
+  public void micro2734() {
+    assertIdentical("{ x <- list(1,list(2,3),4) ; x[[c(2,3)]] <- 3 ; x }", "list(1, list(2, 3, 3), 4)");
+  }
+  @Test
+  public void micro2735() {
+    assertIdentical("{ x <- list(1,list(2,3),4) ; z <- x[[2]] ; x[[c(2,3)]] <- 3 ; z }", "list(2, 3)");
+  }
+  @Test
+  public void micro2736() {
+    assertIdentical("{ x <- list(1,list(2,3),4) ; z <- list(x,x) ; u <- list(z,z) ; u[[c(2,2,3)]] <- 6 ; unlist(u) }", "c(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 6)");
+  }
+  @Test
+  public void micro2737() {
+    assertIdentical("{ f <- function(b,i,v) { b[[i]] <- v ; b } ; f(list(1,2,list(3)), c(3,1), 4) ; f(list(1,2,3), 2L, 3) }", "list(1, 3, 3)");
+  }
+  @Test
+  public void micro2738() {
+    assertIdentical("{ f <- function(b,i,v) { b[[i]] <- v ; b } ; f(list(1,2,list(3)), c(3,1), 4) ; f(list(1,2,3), 2L, NULL) }", "list(1, 3)");
+  }
+  @Test
+  public void micro2739() {
+    assertIdentical("{ f <- function(b,i,v) { b[[i]] <- v ; b } ; f(list(1,2,list(3)), c(3,1), 4) ; f(c(1,2,3), \"hello\", 2) }", "structure(c(1, 2, 3, 2), .Names = c(\"\", \"\", \"\", \"hello\"))");
+  }
+  @Test
+  public void micro2740() {
+    assertIdentical("{ f <- function(b,i,v) { b[[i]] <- v ; b } ; f(list(1,2,b=list(x=3)),c(\"b\",\"x\"),10) }", "structure(list(1, 2, b = structure(list(x = 10), .Names = \"x\")), .Names = c(\"\", \"\", \"b\"))");
+  }
+  @Test
   public void micro2742() {
     assertIdentical("{ f <- function(b,i,v) { b[[i]] <- v ; b } ; f(c(1,2,b=c(x=3)),c(\"b\"),10) }", "structure(c(1, 2, 3, 10), .Names = c(\"\", \"\", \"b.x\", \"b\"))");
+  }
+  @Test
+  public void micro2743() {
+    assertIdentical("{ f <- function(b,i,v) { b[[i]] <- v ; b } ; f(list(1,2,b=list(a=list(x=1,y=2),3),4),c(\"b\",\"a\",\"x\"),10) }", "structure(list(1, 2, b = structure(list(a = structure(list(x = 10,     y = 2), .Names = c(\"x\", \"y\")), 3), .Names = c(\"a\", \"\")),     4), .Names = c(\"\", \"\", \"b\", \"\"))");
+  }
+  @Test
+  public void micro2744() {
+    assertIdentical("{ f <- function(b,i,v) { b[[i]] <- v ; b } ;  f(list(1,2,b=list(a=1)),c(\"b\",\"a\"),10) ; f(list(a=1,b=2),\"b\",NULL) }", "structure(list(a = 1), .Names = \"a\")");
+  }
+  @Test
+  public void micro2745() {
+    assertIdentical("{ f <- function(b,i,v) { b[[i]] <- v ; b } ;  f(list(1,2,b=list(a=1)),c(\"b\",\"a\"),10) ; f(list(a=1,b=list(2)),\"b\",double()) }", "structure(list(a = 1, b = numeric(0)), .Names = c(\"a\", \"b\"))");
+  }
+  @Test
+  public void micro2747() {
+    assertIdentical("{ l <- list(a=1,b=2,cd=list(c=3,d=4)) ; x <- list(l,xy=list(x=l,y=l)) ; x[[c(2,2,3,2)]] <- 10 ; l }", "structure(list(a = 1, b = 2, cd = structure(list(c = 3, d = 4), .Names = c(\"c\", \"d\"))), .Names = c(\"a\", \"b\", \"cd\"))");
+  }
+  @Test //@Ignore("Recursive setting")
+  public void micro2748() {
+    assertIdentical("{ l <- list(a=1,b=2,cd=list(c=3,d=4)) ; x <- list(l,xy=list(x=l,y=l)) ; x[[c(\"xy\",\"y\",\"cd\",\"d\")]] <- 10 ; l }", "structure(list(a = 1, b = 2, cd = structure(list(c = 3, d = 4), .Names = c(\"c\", \"d\"))), .Names = c(\"a\", \"b\", \"cd\"))");
   }
   @Test
   public void micro2749() {
@@ -8838,10 +8922,18 @@ public class MicroTest extends AbstractMicroTest {
   }
   @Test
   public void myTEST2() {
-    assertIdentical("{ x <- list(a = 1, b = list(x = 2, y = 3)); x[['b']][['x']] <- 42; x[['b']][['x']] }", "42");
+    assertIdentical("{ x <- list(a = 1, b = list(x = 2, y = 3)); x[[c('b','x')]] <- 42; x[['b']][['x']] }", "42");
   }
   @Test
   public void myTEST3() {
-    assertIdentical("{ x <- list(a = 1, s = 67, b = list(x = 2, y = 3), c = list(k = 8, g = 7) ); x[['b']][['x']] <- 42; x[['b']][['x']] }", "42");
+    assertIdentical("{ x <- list(a = 1, s = 67, b = list(x = 2, y = 3), c = list(k = 8, m = list(w = list(s = 89), d = 12 ), g = 7) ); x[[c('c','m','d')]] <- 42; x[[c('c','m','d')]] }", "42");
+  }
+  @Test
+  public void myTEST4() {
+    assertIdentical("{ x <- list(a = 1, s = 67, b = list(x = 2, y = 3), c = list(k = 8, m = list(w = list(s = 89), d = 12 ), g = 7) ); x[[c('c','m','w','s')]] <- 42; x[[c('c','m','w','s')]] }", "42");
+  }
+  @Test
+  public void myTEST5() {
+    assertIdentical("{ x <- list(a = 1, s = 67 ); x[['s']] <- 42; x[['s']] }", "42");
   }
 }

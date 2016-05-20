1dd56de747cf7077671b4e9cb5e9dd544bf654e8
Alex Bertram <alex@bedatadriven.com>
2016-05-20 11:32:40 +0200

    gcc-bridge: fixed regression in handling of pointers to record pointers (affects chron package)

---
+++ tools/gcc-bridge/compiler/src/main/java/org/renjin/gcc/codegen/type/record/unit/RecordUnitPtrValueFunction.java
@@ -0,0 +1,55 @@
+package org.renjin.gcc.codegen.type.record.unit;
+
+import com.google.common.base.Optional;
+import org.objectweb.asm.Type;
+import org.renjin.gcc.codegen.expr.Expr;
+import org.renjin.gcc.codegen.expr.Expressions;
+import org.renjin.gcc.codegen.expr.SimpleAddressableExpr;
+import org.renjin.gcc.codegen.expr.SimpleExpr;
+import org.renjin.gcc.codegen.fatptr.FatPtrExpr;
+import org.renjin.gcc.codegen.fatptr.ValueFunction;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RecordUnitPtrValueFunction implements ValueFunction {
+  
+  private Type recordType;
+
+  public RecordUnitPtrValueFunction(Type recordType) {
+    this.recordType = recordType;
+  }
+
+  @Override
+  public Type getValueType() {
+    return recordType;
+  }
+
+  @Override
+  public int getElementLength() {
+    return 1;
+  }
+
+  @Override
+  public int getElementSize() {
+    return 32;
+  }
+
+  @Override
+  public Optional<SimpleExpr> getValueConstructor() {
+    return Optional.absent();
+  }
+
+  @Override
+  public Expr dereference(SimpleExpr array, SimpleExpr offset) {
+    SimpleExpr pointerValue = Expressions.elementAt(array, offset);
+    FatPtrExpr pointerAddress = new FatPtrExpr(array, offset);
+    
+    return new SimpleAddressableExpr(pointerValue, pointerAddress);
+  }
+
+  @Override
+  public List<SimpleExpr> toArrayValues(Expr expr) {
+    return Collections.singletonList((SimpleExpr)expr);
+  }
+}

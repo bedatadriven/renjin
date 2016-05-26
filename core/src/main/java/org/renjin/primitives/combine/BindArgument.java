429d543d9cdc51ab7657ef9e8cc702ed56aaf5d5
Parham Solaimani <parham@bedatadriven.com>
2016-05-26 16:29:30 +0200

    refactor BindArgument(), Matrix2dBuilder() to top level classes (issue #135)

---
+++ core/src/main/java/org/renjin/primitives/combine/BindArgument.java
@@ -0,0 +1,51 @@
+package org.renjin.primitives.combine;
+
+import org.renjin.sexp.*;
+
+/**
+ * Bind vector/matrix dims and dimnames
+ */
+class BindArgument {
+  final Vector vector;
+  final int rows;
+  final int cols;
+
+  AtomicVector rowNames = Null.INSTANCE;
+  AtomicVector colNames = Null.INSTANCE;
+
+  /**
+   * True if the argument is an actual matrix
+   */
+  final boolean matrix;
+  String argName;
+
+  public BindArgument(String argName, Vector vector, boolean defaultToRows) {
+    this.argName = argName;
+    SEXP dim = vector.getAttributes().getDim();
+    this.vector = vector;
+    if (dim == Null.INSTANCE || dim.length() != 2) {
+      if (defaultToRows) {
+        rows = 1;
+        cols = vector.length();
+        colNames = vector.getNames();
+      } else {
+        cols = 1;
+        rows = vector.length();
+        rowNames = vector.getNames();
+      }
+      matrix = false;
+
+    } else {
+      AtomicVector dimVector = (AtomicVector) dim;
+      rows = dimVector.getElementAsInt(0);
+      cols = dimVector.getElementAsInt(1);
+      Vector dimNames = (Vector) this.vector.getAttribute(Symbols.DIMNAMES);
+      if (dimNames instanceof ListVector && dimNames.length() == 2) {
+        rowNames = dimNames.getElementAsSEXP(0);
+        colNames = dimNames.getElementAsSEXP(1);
+      }
+
+      matrix = true;
+    }
+  }
+}

1a0af883589232b20d0689d302ee7325ad9359dd
Parham Solaimani <parham@bedatadriven.com>
2016-05-27 15:43:07 +0200

    move .get_internal_S3_generics codes to correct locations

--- packages/tools/src/test/R/test.R
+++ packages/tools/src/test/R/test.R
@@ -12,3 +12,9 @@ test.md5 <- function() {
 }
 
 
+test.utils.functions <- function() {
+    res <- tools:::.get_internal_S3_generics()
+
+    assertThat(typeof(res), identicalTo("character"))
+    assertTrue(length(res) > 1)
+}
--- tests/src/test/R/utils.R
+++
@@ -1,8 +0,0 @@
-library(hamcrest)
-
-test.utils.functions <- function() {
-    res <- tools:::.get_internal_S3_generics()
-
-    assertThat(typeof(res), identicalTo("character"))
-    assertTrue(length(res) > 1)
-}

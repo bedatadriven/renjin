9bfab53c2d01e58058a2ed6bc79e37d2f0426ccb
Parham Solaimani <parham@bedatadriven.com>
2016-08-01 19:12:27 +0200

    use as extensive as possible init.c for graphics

--- packages/graphics/src/main/c/init.c
+++ packages/graphics/src/main/c/init.c
@@ -36,6 +36,47 @@ static const R_CallMethodDef CallEntries[] = {
 };
 
 
+#define EXTDEF(name, n)  {#name, (DL_FUNC) &name, n}
+
+static const R_ExternalMethodDef ExtEntries[] = {
+    EXTDEF(C_contour, -1),
+    EXTDEF(C_filledcontour, 5),
+    EXTDEF(C_image, 4),
+    EXTDEF(C_persp, -1),
+
+    EXTDEF(C_abline, -1),
+    EXTDEF(C_axis, -1),
+    EXTDEF(C_arrows, -1),
+    EXTDEF(C_box, -1),
+    EXTDEF(C_clip, -1),
+    EXTDEF(C_convertX, 3),
+    EXTDEF(C_convertY, 3),
+    EXTDEF(C_dend, -1),
+    EXTDEF(C_dendwindow, -1),
+    EXTDEF(C_erase, -1),
+    EXTDEF(C_layout, -1),
+    EXTDEF(C_mtext, -1),
+    EXTDEF(C_par, -1),
+    EXTDEF(C_path, -1),
+    EXTDEF(C_plotXY, -1),
+    EXTDEF(C_plot_window, -1),
+    EXTDEF(C_polygon, -1),
+    EXTDEF(C_raster, -1),
+    EXTDEF(C_rect, -1),
+    EXTDEF(C_segments, -1),
+    EXTDEF(C_strHeight, -1),
+    EXTDEF(C_strWidth, -1),
+    EXTDEF(C_symbols, -1),
+    EXTDEF(C_text, -1),
+    EXTDEF(C_title, -1),
+    EXTDEF(C_xspline, -1),
+
+    EXTDEF(C_plot_new, 0),
+    EXTDEF(C_locator, -1),
+    EXTDEF(C_identify, -1),
+    {NULL, NULL, 0}
+};
+
 
 void
 #ifdef HAVE_VISIBILITY_ATTRIBUTE
@@ -43,8 +84,7 @@ __attribute__ ((visibility ("default")))
 #endif
 R_init_graphics(DllInfo *dll)
 {
-    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_registerRoutines(dll, NULL, CallEntries, NULL, ExtEntries);
     R_useDynamicSymbols(dll, FALSE);
     R_forceSymbols(dll, TRUE);
-    //registerBase();
 }

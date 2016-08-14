423a9e75b55e3c9941056481eac7aeb1b3f4be37
Kang Tu <tninja@gmail.com>
2016-08-14 18:26:22 +0000

    stage current changes

---
+++ packages/stats/src/test/R/test.stats.arima.R
@@ -0,0 +1,6 @@
+library(hamcrest)
+
+test.arima <- function() {
+    ## fit <- arima(lh, order = c(3,0,0), optim.method="Nelder-Mead")
+    ## assertThat(round(coef(fit)['ar1'], 3), equalTo(0.644))
+}

77fbee4b68a0fec15078fa71ee752bfdf972748f
Alex Bertram <alex@bedatadriven.com>
2016-09-23 14:44:07 +0200

    Excluding third-party files from license header update

--- core/src/main/java/org/renjin/stats/internals/optimize/Blas_f77.java
+++ core/src/main/java/org/renjin/stats/internals/optimize/Blas_f77.java
@@ -1,27 +1,4 @@
 /*
- * R : A Computer Language for Statistical Data Analysis
- * Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
- * Copyright (C) 1997--2008  The R Development Core Team
- * Copyright (C) 2003, 2004  The R Foundation
- * Copyright (C) 2010 bedatadriven
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-package org.renjin.stats.internals.optimize;
-
-/*
     Blas_f77.java copyright claim:
 
     This software is based on public domain LINPACK routines.
@@ -69,6 +46,9 @@ Date        Translator        Changes
 3/10/98     Steve Verrill     isamax and colisamax added
 
 */
+
+package org.renjin.stats.internals.optimize;
+
 
 
 /**

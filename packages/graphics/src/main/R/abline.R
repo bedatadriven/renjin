753ebcfb3d349c727d9606cc73ec642bbdbae6e4
Parham Solaimani <parham@bedatadriven.com>
2016-09-30 11:48:50 +0200

    stub out abline, polygon, and rect graphics functions

--- packages/graphics/src/main/R/abline.R
+++ packages/graphics/src/main/R/abline.R
@@ -17,34 +17,7 @@
 abline <- function(a = NULL, b = NULL, h = NULL, v = NULL, reg = NULL,
                    coef = NULL, untf = FALSE, ...)
 {
-    int_abline <- function(a, b, h, v, untf, col = par("col"),
-                           lty = par("lty"), lwd = par("lwd"), ...)
-        .Internal(abline(a, b, h, v, untf, col, lty, lwd, ...))
-
-    if(!is.null(reg)) {
-        if(!is.null(a)) warning("'a' is overridden by 'reg'")
-        a <- reg
+    {
+        cat("Sorry, we haven't implemented much of the graphics stuff. Looking for volunteers!\n")
     }
-    if(is.object(a) || is.list(a)) {
-	## was	(!is.null(a) && is.list(a))
-	p <- length(coefa <- as.vector(coef(a)))
-	if (p > 2)
-            warning(gettextf("only using the first two of %d regression coefficients", p), domain = NA)
-	islm <- inherits(a, "lm")
-	noInt <- if(islm) !as.logical(attr(stats::terms(a), "intercept")) else p == 1
-	if (noInt) {
-	    a <- 0
-	    b <- coefa[1L]
-	} else {
-	    a <- coefa[1L]
-	    b <- if (p >= 2) coefa[2L] else 0
-	}
-    }
-    if(!is.null(coef)) {
-	if(!is.null(a)) warning("'a' and 'b' are overridden by 'coef'")
-	a <- coef[1L]
-	b <- coef[2L]
-    }
-    int_abline(a=a, b=b, h=h, v=v, untf=untf, ...)
-    invisible()
 }

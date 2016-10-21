53712d2f307d188cfb20bceae7b924161d52c265
Alex Bertram <alex@bedatadriven.com>
2016-10-21 09:05:35 +0200

    Provide isElementTrue() override for LogicalArrayVector

--- core/src/main/java/org/renjin/sexp/LogicalArrayVector.java
+++ core/src/main/java/org/renjin/sexp/LogicalArrayVector.java
@@ -103,6 +103,11 @@ public class LogicalArrayVector extends LogicalVector {
   }
 
   @Override
+  public boolean isElementTrue(int index) {
+    return values[index] == 1;
+  }
+
+  @Override
   public boolean isConstantAccessTime() {
     return true;
   }

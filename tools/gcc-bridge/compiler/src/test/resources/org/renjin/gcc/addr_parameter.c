f8e3eec55b53656ae5c365f709de1331dcf5ef06
Parham Solaimani <parham@bedatadriven.com>
2017-03-16 09:49:41 +0100

    fix handeling addressable pointers as argument

--- tools/gcc-bridge/compiler/src/test/resources/org/renjin/gcc/addr_parameter.c
+++ tools/gcc-bridge/compiler/src/test/resources/org/renjin/gcc/addr_parameter.c
@@ -1,29 +1,50 @@
 #include "assert.h"
+
+struct SS {
+    int a;
+    double b;
+    char c;
+};
+
 struct S {
     int x;
     double y;
+    struct SS z;
 };
 
-void function2(struct S *x) {
-    struct S **b = &x;
-    struct S c;
-    c.x = 45;
-    c.y = 76;
-    x = &c;
-    ASSERT((*x).x == 45);
-    ASSERT((*x).y == 76);
+void addressed_param(struct S *ps) {
+
+    ASSERT(ps->x == 41);
+    ASSERT(ps->z.a == 37);
+
+    struct S **pps = &ps;
+    ASSERT((*pps)->x == 41);
+
+    struct S *ps2 = ps;
+    struct S s;
+    ps = &s;
+    ps->x = 42;
+
+    ASSERT((*pps)->x == 42);
+    ASSERT(ps2->x == 41);
 }
 
-void addressed_param(struct S *s, int x, double y) {
-    struct S *z = s;
-    (*z).y = (double)x*y;
-    ASSERT((*s).x == 41);
-    ASSERT((*s).y == 17.5);
-    function2(z);
-}
 void test_param() {
     struct S s;
     s.x = 41;
     s.y = 1.2;
-    addressed_param(&s, 5, 3.5);
+    s.z.a = 37;
+    addressed_param(&s);
 }
+
+void test_param2() {
+    struct S s;
+    s.x = 41;
+    s.y = 1.2;
+    s.z.a = 37;
+    struct S s2;
+    s2 = s;
+    s2.z.a = 27;
+    ASSERT(s.z.a == 37);
+}
+

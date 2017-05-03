f3536c0708c17755ab613b46f582bb3376ddac51
Parham Solaimani <parham@bedatadriven.com>
2017-05-03 15:37:43 +0200

    brake down complex test case for distance based method selection

--- test-packages/s4test/src/test/R/test.s4.method.selection.00.R
+++ test-packages/s4test/src/test/R/test.s4.method.selection.00.R
@@ -54,16 +54,34 @@ test.method_selection.03 = function() {
 
 }
 
-ignore.test.method_selection.00 = function() {
+ignore.test.method_selection.03a1 = function() {
+    setMethod("[", signature("B","C","C"), function(x, i, j, ...) 3.5)
+    assertThat(a[a,a], identicalTo( c(3.5) ))
+}
 
+ignore.test.method_selection.03a2 = function() {
     setMethod("[", signature("B","C","C"), function(x, i, j, ...) 3.5)
     setMethod("[", signature("B","D","B"), function(x, i, j, ...) 5.5)
-
     assertThat(a[a,a], identicalTo( c(3.5) ))
-    assertThat(b[b,b], identicalTo( c(3.5) ))
-    assertThat(a[c,c], identicalTo( c(3.5) ))
-    assertThat(a[a,c], identicalTo( c(3.5) ))
-    assertThat(a[a,b], identicalTo( c(5.5) ))
-    assertThat(a[b,b], identicalTo( c(5.5) ))
+}
 
+ignore.test.method_selection.03b = function() {
+    assertThat(b[b,b], identicalTo( c(3.5) ))
+}
+
+ignore.test.method_selection.03c = function() {
+    assertThat(a[c,c], identicalTo( c(3.5) ))
+}
+
+ignore.test.method_selection.03d = function() {
+    assertThat(a[a,c], identicalTo( c(3.5) ))
+}
+
+ignore.test.method_selection.03e = function() {
+    assertThat(a[a,b], identicalTo( c(5.5) ))
+
+}
+
+ignore.test.method_selection.03f = function() {
+    assertThat(a[b,b], identicalTo( c(5.5) ))
 }

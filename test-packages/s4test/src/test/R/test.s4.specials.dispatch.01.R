9b6985228603e6d63328d7d4a6d5b056af60d5bf
Parham Solaimani <parham@bedatadriven.com>
2017-05-17 14:11:44 +0200

    simple test case for S4 dispatch to special functions (failing)

---
+++ test-packages/s4test/src/test/R/test.s4.specials.dispatch.01.R
@@ -0,0 +1,29 @@
+#
+# Renjin : JVM-based interpreter for the R language for the statistical analysis
+# Copyright © 2010-2016 BeDataDriven Groep B.V. and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, a copy is available at
+# https://www.gnu.org/licenses/gpl-2.0.txt
+#
+
+library(hamcrest)
+library(methods)
+
+
+fg <- setRefClass("foo", c("bar", "flag"))
+f1 <- fg(flag = "testing")
+ignore.test.dispatch.specials.001 = function() {
+    f1$bar <- 1
+    assertThat(f1$bar, identicalTo(1))
+}

b85b46a3aafe00f9d79006a2ff0eaceeecdc37b0
Parham Solaimani <parham@bedatadriven.com>
2017-05-25 13:29:37 +0200

    add test for class S3Part of S4 object

--- packages/methods/src/test/R/testS3.R
+++ packages/methods/src/test/R/testS3.R
@@ -16,6 +16,12 @@ xx <- data.frame(a=rnorm(3),
 setClass("myData", representation(extra = "character"), contains = "data.frame")
 mx <- new("myData", xx, extra = "testing")
 
+test.class.s3part.s4object = function() {
+    assertThat(S3Class(S3Part(mx)), identicalTo("data.frame"))
+    assertThat(S3Class(S3Part(mx, TRUE)), identicalTo("data.frame"))
+}
+
+
 # three kinds of $<-: replace, add, delete (NULL value)
 mx$a <- mx$a * 2; mx$c <- 1:3; mx$b <- NULL
 xx$a <- xx$a * 2; xx$c <- 1:3; xx$b <- NULL
@@ -27,9 +33,14 @@ test.relationS3toS4.01 <- function(){
 }
 
 tx <- mx
+
+
+
+# test addition of vector as a data.frame column
 ChrVectr <- c("A", "B", "C"); IntVectr <- 10:8; DblVectr <- c(0.1, 0.2, 0.3); LgcVectr <- c(TRUE, TRUE, FALSE)
-Chr <- "ABC"; Int <- 5L; Dbl <- 1.5; Lgc <- TRUE
 tx$ChrVectr <- ChrVectr; tx$IntVectr <- IntVectr; tx$DblVectr <- DblVectr; tx$LgcVectr <- LgcVectr;
+# test addition and extension of single value as a data.frame column
+Chr <- "ABC"; Int <- 5L; Dbl <- 1.5; Lgc <- TRUE
 tx$Chr <- Chr; tx$Int <- Int; tx$Dbl <- Dbl; tx$Lgc <- Lgc;
 
 xy <- cbind(xx, ChrVectr = ChrVectr, IntVectr = IntVectr, DblVectr = DblVectr, LgcVectr = LgcVectr,

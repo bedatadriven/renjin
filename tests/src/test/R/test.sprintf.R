042181e70b9cd66a0593d0be92200d54234cdb00
Alex Bertram <alex@bedatadriven.com>
2017-06-08 15:25:24 +0200

    sprintf: Allow floating point formatters for integers

---
+++ tests/src/test/R/test.sprintf.R
@@ -0,0 +1,32 @@
+#
+# Renjin : JVM-based interpreter for the R language for the statistical analysis
+# Copyright © 2010-2016 BeDataDriven Groep B.V. and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, a copy is available at
+# https://www.gnu.org/licenses/gpl-2.0.txt
+#
+
+library(hamcrest)
+
+assertThat(sprintf("%i", 1L), identicalTo("1"))
+assertThat(sprintf("%f", 1L), identicalTo("1.000000"))
+assertThat(sprintf("%E", 1L), identicalTo("1.000000E+00"))
+assertThat(sprintf("%e", 1L), identicalTo("1.000000e+00"))
+assertThat(sprintf("%g", 1L), identicalTo("1"))
+assertThat(sprintf("%G", 1L), identicalTo("1"))
+
+assertThat(sprintf("%g", 102400000L), identicalTo("1.024e+08"))
+assertThat(sprintf("%G", 102400000L), identicalTo("1.024E+08"))
+
+

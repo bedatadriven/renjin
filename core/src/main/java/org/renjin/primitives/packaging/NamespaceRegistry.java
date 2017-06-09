6db424c6add5411c7b5afcb6e2311d9f8920e8f7
Alex Bertram <alex@bedatadriven.com>
2017-06-09 09:51:33 +0200

    Fix for older packages: call .First.lib instead/addition to .onLoad if present

--- core/src/main/java/org/renjin/primitives/packaging/NamespaceRegistry.java
+++ core/src/main/java/org/renjin/primitives/packaging/NamespaceRegistry.java
@@ -205,11 +205,8 @@ public class NamespaceRegistry {
 
         // invoke the .onLoad hook
         // (Before we export symbols!)
-        if(namespace.getNamespaceEnvironment().hasVariable(Symbol.get(".onLoad"))) {
-          StringVector nameArgument = StringVector.valueOf(pkg.getName().getPackageName());
-          context.evaluate(FunctionCall.newCall(Symbol.get(".onLoad"), nameArgument, nameArgument),
-              namespace.getNamespaceEnvironment());
-        }
+        invokeOnLoadFunction(context, ".First.lib", pkg, namespace);  // Deprecated
+        invokeOnLoadFunction(context, ".onLoad", pkg, namespace);
 
         // finally export symbols from the namespace
         namespace.initExports(namespaceFile);
@@ -220,6 +217,14 @@ public class NamespaceRegistry {
       } catch(Exception e) {
         throw new EvalException("IOException while loading package " + fqName + ": " + e.getMessage(), e);
       }
+    }
+  }
+
+  private void invokeOnLoadFunction(Context context, String functionName, Package pkg, Namespace namespace) {
+    if(namespace.getNamespaceEnvironment().hasVariable(Symbol.get(functionName))) {
+      StringVector nameArgument = StringVector.valueOf(pkg.getName().getPackageName());
+      context.evaluate(FunctionCall.newCall(Symbol.get(functionName), nameArgument, nameArgument),
+          namespace.getNamespaceEnvironment());
     }
   }
 

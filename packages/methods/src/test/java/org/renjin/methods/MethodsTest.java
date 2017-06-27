a94a5da1a00582a7f24f6ab16612fb86df7c1579
Parham Solaimani <parham@bedatadriven.com>
2017-06-27 15:14:09 +0200

    add test case for loop compiler S4 handling in methods package

--- packages/methods/src/test/java/org/renjin/methods/MethodsTest.java
+++ packages/methods/src/test/java/org/renjin/methods/MethodsTest.java
@@ -8,6 +8,8 @@ import org.renjin.eval.EvalException;
 import org.renjin.eval.Session;
 import org.renjin.eval.SessionBuilder;
 import org.renjin.parser.RParser;
+import org.renjin.primitives.special.ForFunction;
+import org.renjin.sexp.DoubleArrayVector;
 import org.renjin.sexp.ExpressionVector;
 import org.renjin.sexp.IntVector;
 import org.renjin.sexp.SEXP;
@@ -59,5 +61,38 @@ public class MethodsTest {
 
     assertThat(dim.getElementAsInt(0), equalTo(12));
     assertThat(dim.getElementAsInt(1), equalTo(1));
+  }
+  
+  @Ignore
+  public void loopS4MethodCall() throws IOException {
+  
+    ForFunction.COMPILE_LOOPS = false;
+//    String rdsFile = Resources.getResource("100k_36bp_Seqs_list.rds").getFile();
+//    global.setVariable(topLevelContext, "file" , new StringArrayVector(rdsFile));
+//    eval("y <- readRDS(file)");
+//    eval("library(methods)");
+    eval(" set.seed(101)                                                                   ");
+    eval(" setClass('Seq', representation(seq = 'character') )                             ");
+    eval(" reads=character(10000)                                                          ");
+    eval(" aa=c('A','T','C','G')                                                           ");
+    eval(" seqs=list()                                                                     ");
+    eval(" for(i in 1:1e4) reads[i] <- paste0(sample(aa, 36, replace = TRUE), collapse='') ");
+    eval(" for(i in 1:1e4) seqs[[i]] <- new('Seq', seq=reads[i])                           ");
+    eval(" setClass('SeqSum', representation(seq = 'character', value = 'numeric') )       ");
+    eval(" setMethod('+', signature(e1 = 'SeqSum', e2 = 'Seq'), function(e1, e2) { hasSeq <- grep(e1@seq, e2@seq); if(length(hasSeq) > 0) e1@value <- e1@value + 1; return(e1) }) ");
+    eval(" atg <- new('SeqSum', seq = 'ATG', value = 0)                                    ");
+    ForFunction.COMPILE_LOOPS = true;
+    eval(" for(i in 1:1e4) { atg <- atg + seqs[[ i ]] }                                    ");
+//    eval(" x <- letters;x=c(x,x,x);x=c(x,x,x);x=c(x,x,x);x=c(x,x,x,x);x=c(x,x,x,x);       ");
+//    eval(" for(i in 1:1e4) { atg <- paste0('10k x: ', x[i]) }                         ");
+    eval("                                                                                 ");
+  
+    ForFunction.COMPILE_LOOPS = false;
+  
+    DoubleArrayVector count = (DoubleArrayVector) eval("atg@value");
+    assertThat(count.getElementAsInt(0), equalTo( 4316 ));
+    
+//    StringArrayVector count = (StringArrayVector) eval("atg");
+//    assertThat(count.getElementAsString(0), equalTo( "10k x: p" ));
   }
 }

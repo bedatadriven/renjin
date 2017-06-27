868d407bdb206d084ebf6b37212f8e15a3290545
Parham Solaimani <parham@bedatadriven.com>
2017-06-27 18:58:07 +0200

    simpler test case for loop compiler and S4 dispatch

--- packages/methods/src/test/java/org/renjin/methods/MethodsTest.java
+++ packages/methods/src/test/java/org/renjin/methods/MethodsTest.java
@@ -66,11 +66,7 @@ public class MethodsTest {
   @Ignore
   public void loopS4MethodCall() throws IOException {
   
-    ForFunction.COMPILE_LOOPS = false;
-//    String rdsFile = Resources.getResource("100k_36bp_Seqs_list.rds").getFile();
-//    global.setVariable(topLevelContext, "file" , new StringArrayVector(rdsFile));
-//    eval("y <- readRDS(file)");
-//    eval("library(methods)");
+    ForFunction.COMPILE_LOOPS = true;
     eval(" set.seed(101)                                                                   ");
     eval(" setClass('Seq', representation(seq = 'character') )                             ");
     eval(" reads=character(10000)                                                          ");
@@ -81,18 +77,33 @@ public class MethodsTest {
     eval(" setClass('SeqSum', representation(seq = 'character', value = 'numeric') )       ");
     eval(" setMethod('+', signature(e1 = 'SeqSum', e2 = 'Seq'), function(e1, e2) { hasSeq <- grep(e1@seq, e2@seq); if(length(hasSeq) > 0) e1@value <- e1@value + 1; return(e1) }) ");
     eval(" atg <- new('SeqSum', seq = 'ATG', value = 0)                                    ");
-    ForFunction.COMPILE_LOOPS = true;
     eval(" for(i in 1:1e4) { atg <- atg + seqs[[ i ]] }                                    ");
-//    eval(" x <- letters;x=c(x,x,x);x=c(x,x,x);x=c(x,x,x);x=c(x,x,x,x);x=c(x,x,x,x);       ");
-//    eval(" for(i in 1:1e4) { atg <- paste0('10k x: ', x[i]) }                         ");
     eval("                                                                                 ");
   
     ForFunction.COMPILE_LOOPS = false;
   
     DoubleArrayVector count = (DoubleArrayVector) eval("atg@value");
-    assertThat(count.getElementAsInt(0), equalTo( 4316 ));
+    assertThat(count.getElementAsInt(0), equalTo(4316));
+  }
+  
+  @Ignore
+  public void loopS4MethodCall2() throws IOException {
+  
+    ForFunction.COMPILE_LOOPS = true;
     
-//    StringArrayVector count = (StringArrayVector) eval("atg");
-//    assertThat(count.getElementAsString(0), equalTo( "10k x: p" ));
+    eval(" set.seed(101); ");
+    eval(" setClass('SeqLengthMean', representation(name='character', value='numeric', count='numeric') );   ");
+    eval(" setMethod('+', signature(e1='SeqLengthMean', e2='numeric'), function(e1, e2) { c<-e1@count+1.0; v<-e1@value; e1@value<-v+((e2-v)/(c)); e1@count<-c; return(e1) }) ");
+    eval(" meanLength <- new('SeqLengthMean', name='Mean length', value=0.0, count=0.0); ");
+    eval(" lengths <- sample(100:3000, 1e6, replace = TRUE);");
+    eval(" lengths <- as.numeric(lengths);");
+    eval(" for(i in 1:1e6) { meanLength <- meanLength + lengths[i] } ");
+    eval("  ");
+    // print warnings: session.getTopLevelContext()
+  
+    DoubleArrayVector value = (DoubleArrayVector) eval("meanLength@value");
+    DoubleArrayVector count = (DoubleArrayVector) eval("meanLength@count");
+    assertThat(count.getElementAsDouble(0), equalTo( 1e6 ));
+    assertThat(value.getElementAsDouble(0), equalTo( 1550.4922619999654 ));
   }
 }

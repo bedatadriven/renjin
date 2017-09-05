0646e9c3cd4651501097ab850a9f03631244376d
Alex Bertram <alex@bedatadriven.com>
2017-09-05 09:41:41 +0200

    Imported stats::acf sources from GNU R 3.4.0

--- packages/stats/src/main/R/acf.R
+++ packages/stats/src/main/R/acf.R
@@ -1,5 +1,7 @@
 #  File src/library/stats/R/acf.R
-#  Part of the R package, http://www.R-project.org
+#  Part of the R package, https://www.R-project.org
+#
+#  Copyright (C) 1999-2015 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -12,17 +14,18 @@
 #  GNU General Public License for more details.
 #
 #  A copy of the GNU General Public License is available at
-#  http://www.r-project.org/Licenses/
+#  https://www.R-project.org/Licenses/
 
 acf <-
     function (x, lag.max = NULL,
               type = c("correlation", "covariance", "partial"),
-              plot = TRUE, na.action = na.fail, demean= TRUE, ...)
+              plot = TRUE, na.action = na.fail, demean = TRUE, ...)
 {
     type <- match.arg(type)
     if(type == "partial") {
         m <- match.call()
-        m[[1L]] <- as.name("pacf")
+        ## need stats:: for non-standard evaluation
+        m[[1L]] <- quote(stats::pacf)
         m$type <- NULL
         return(eval(m, parent.frame()))
     }
@@ -32,29 +35,28 @@ acf <-
     x <- as.matrix(x)
     if(!is.numeric(x))
         stop("'x' must be numeric")
-    sampleT <- nrow(x)
-    nser <- ncol(x)
+    sampleT <- as.integer(nrow(x))
+    nser <- as.integer(ncol(x))
+    if(is.na(sampleT) || is.na(nser))
+        stop("'sampleT' and 'nser' must be integer")
     if (is.null(lag.max))
         lag.max <- floor(10 * (log10(sampleT) - log10(nser)))
-    lag.max <- min(lag.max, sampleT - 1)
-    if (lag.max < 0) stop("'lag.max' must be at least 0")
+    lag.max <- as.integer(min(lag.max, sampleT - 1L))
+    if (is.na(lag.max) || lag.max < 0)
+        stop("'lag.max' must be at least 0")
     if(demean)
 	x <- sweep(x, 2, colMeans(x, na.rm = TRUE), check.margin=FALSE)
     lag <- matrix(1, nser, nser)
     lag[lower.tri(lag)] <- -1
-    acf <- array(.C(C_acf,
-                    as.double(x), as.integer(sampleT), as.integer(nser),
-                    as.integer(lag.max), as.integer(type=="correlation"),
-                    acf=double((lag.max+1L) * nser * nser), NAOK = TRUE
-                    )$acf, c(lag.max + 1L, nser, nser))
+    acf <- .Call(C_acf, x, lag.max, type == "correlation")
     lag <- outer(0:lag.max, lag/x.freq)
-    acf.out <- structure(.Data = list(acf = acf, type = type,
-        n.used = sampleT, lag = lag, series = series, snames = colnames(x)),
-        class = "acf")
+    acf.out <- structure(list(acf = acf, type = type, n.used = sampleT,
+                              lag = lag, series = series, snames = colnames(x)),
+                         class = "acf")
     if (plot) {
         plot.acf(acf.out, ...)
-        return(invisible(acf.out))
-    } else return(acf.out)
+        invisible(acf.out)
+    } else acf.out
 }
 
 pacf <- function(x, lag.max, plot, na.action, ...) UseMethod("pacf")
@@ -74,7 +76,7 @@ pacf.default <- function(x, lag.max = NULL, plot = TRUE,
     if (lag.max < 1) stop("'lag.max' must be at least 1")
 
     if(is.matrix(x)) {
-        if(any(is.na(x))) stop("NAs in 'x'")
+        if(anyNA(x)) stop("NAs in 'x'")
         nser <- ncol(x)
         x <- sweep(x, 2, colMeans(x), check.margin=FALSE)
         lag <- matrix(1, nser, nser)
@@ -86,11 +88,7 @@ pacf.default <- function(x, lag.max = NULL, plot = TRUE,
         x <- scale(x, TRUE, FALSE)
         acf <- drop(acf(x, lag.max = lag.max, plot = FALSE,
                         na.action = na.action)$acf)
-        pacf <- array(.C(C_uni_pacf,
-                         as.double(acf),
-                         pacf = double(lag.max),
-                         as.integer(lag.max))$pacf,
-                      dim=c(lag.max,1L,1L))
+        pacf <- .Call(C_pacf1, acf, lag.max)
         lag <- array((1L:lag.max)/x.freq, dim=c(lag.max,1L,1L))
         snames <- NULL
     }
@@ -176,9 +174,7 @@ plot.acf <-
         iind <- (I-1)*nr + 1L:nr
         jind <- (J-1)*nr + 1L:nr
         if(verbose)
-            message("Page [",I,",",J,"]: i =",
-                    paste(iind,collapse=","),"; j =",
-                    paste(jind,collapse=","), domain = NA)
+            message(gettextf("Page [%d,%d]: i =%s; j =%s", I, J, paste(iind,collapse=","), paste(jind,collapse=",")), domain = NA)
         for (i in iind) for (j in jind)
             if(max(i,j) > nser) {
                 frame(); box(col = "light gray")
@@ -244,24 +240,23 @@ ccf <- function(x, y, lag.max = NULL,
     x
 }
 
-print.acf <- function(x, digits=3, ...)
+print.acf <- function(x, digits = 3L, ...)
 {
     type <- match(x$type, c("correlation", "covariance", "partial"))
     msg <- c("Autocorrelations", "Autocovariances", "Partial autocorrelations")
     cat("\n", msg[type]," of series ", sQuote(x$series), ", by lag\n\n",
-        sep="")
+        sep = "")
     nser <- ncol(x$lag)
     if(type != 2) x$acf <- round(x$acf, digits)
     if(nser == 1) {
-        acfs <- drop(x$acf)
-        names(acfs) <- format(drop(x$lag), digits=3)
+        acfs <- setNames(drop(x$acf), format(drop(x$lag), digits = 3L))
         print(acfs, digits = digits, ...)
     } else {
         acfs <- format(x$acf, ...)
-        lags <- format(x$lag, digits=3)
-        acfs <- array(paste(acfs, " (", lags, ")", sep=""), dim=dim(x$acf))
+        lags <- format(x$lag, digits = 3L)
+        acfs <- array(paste0(acfs, " (", lags, ")"), dim = dim(x$acf))
         dimnames(acfs)  <- list(rep("", nrow(x$lag)), x$snames, x$snames)
-        print(acfs, quote=FALSE, ...)
+        print(acfs, quote = FALSE, ...)
     }
     invisible(x)
 }

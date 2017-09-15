bcea99267980c5ea0c68dbfe672e087e670e4b92
Alex Bertram <alex@bedatadriven.com>
2017-09-15 14:57:18 +0200

    Updated missing license headers

--- tests/src/test/R/test.c.R
+++ tests/src/test/R/test.c.R
@@ -3,7 +3,7 @@
 # Copyright © 2010-2016 BeDataDriven Groep B.V. and contributors
 #
 # This program is free software; you can redistribute it and/or modify
-# it under the terms, of the GNU General Public License as published by
+# it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #

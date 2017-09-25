bedatadriven/renjin

commit f880748ffaf09ac537884148bf66b2786ce325f9

Fixes #290: Convert paste() arguments with as.character()

core/src/main/java/org/renjin/primitives/text/Text.java
@@ -33,6 +33,7 @@ import org.renjin.sexp.*;
 
 import java.nio.charset.Charset;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -44,15 +45,28 @@ public class Text {
   private Text() {}
 
   @Internal
-  public static StringVector paste(ListVector arguments, String separator, String collapse) {
+  public static StringVector paste(@Current Context context, ListVector arguments, String separator, String collapse) {
 
     int resultLength = arguments.maxElementLength();
+
+    List<StringVector> argumentVectors = new ArrayList<>();
+    for (SEXP argument : arguments) {
+      if(argument instanceof StringVector) {
+        argumentVectors.add((StringVector) argument);
+      } else {
+        SEXP result = context.evaluate(FunctionCall.newCall(Symbol.get("as.character"), argument));
+        if(!(result instanceof StringVector)) {
+          throw new EvalException("as.character() returned non-character");
+        }
+        argumentVectors.add((StringVector) result);
+      }
+    }
 
     if(collapse == null) {
       String results[] = new String[resultLength];
       for(int index=0; index!=resultLength; ++index) {
         results[index] = Joiner.on(separator).join(
-            transform(arguments, new StringElementAt(index)));
+            transform(argumentVectors, new StringElementAt(index)));
       }
       return new StringArrayVector( results );
 
@@ -63,7 +77,7 @@ public class Text {
           result.append(collapse);
         }
         Joiner.on(separator).appendTo(result,
-            transform(arguments, new StringElementAt(index)));
+            transform(argumentVectors, new StringElementAt(index)));
       }
       return StringVector.valueOf(result.toString());
     }
@@ -74,11 +88,6 @@ public class Text {
       int justify, boolean naEncode) {
     
     return x;
-  }
-  
-  @Internal("file.path")
-  public static StringVector filePath(ListVector components, String fileSeparator) {
-    return paste(components, fileSeparator, null);
   }
 
   @Internal

tests/src/test/R/test.paste.R
@@ -30,3 +30,9 @@ test.paste <- function() {
 }
 
 
+test.paste.factors <- function() {
+
+    x <- as.factor(c("Geothermal", "Electricity"))
+
+    assertThat(paste(x[1], x[2]), identicalTo("Geothermal Electricity"))
+}

core/src/main/R/base/files.R
@@ -90,7 +90,7 @@ list.dirs <- function(path = ".", full.names = TRUE, recursive = TRUE)
 
 file.path <-
 function(..., fsep=.Platform$file.sep)
-    .Internal(file.path(list(...), fsep))
+    paste(list(...), collapse = fsep)
 
 
 file.exists <- function(...) .Internal(file.exists(c(...)))

core/src/main/java/org/renjin/primitives/Primitives.java
@@ -584,7 +584,6 @@ public class Primitives {
     f("as.raw", Vectors.class, 1);
     f("as.vector", Vectors.class, 11);
     f("paste", Text.class, 11);
-    f("file.path", Text.class, 11);
     f("format", Text.class, 11);
     f("format.info", /*formatinfo*/ null, 11);
     f("cat", Cat.class, 111);

commit bab324ec802b659521c9467b2227afc69fb3c737

Fixes handling of symbols in paste()
Fixes regression introduced by f880748ffaf09ac537884148bf66b2786ce325f9

core/src/main/java/org/renjin/primitives/text/Text.java
@@ -55,7 +55,7 @@ public class Text {
       if(argument instanceof StringVector) {
         argumentVectors.add((StringVector) argument);
       } else {
-        SEXP result = context.evaluate(FunctionCall.newCall(Symbol.get("as.character"), argument));
+        SEXP result = context.evaluate(FunctionCall.newCall(Symbol.get("as.character"), Promise.repromise(argument)));
         if(!(result instanceof StringVector)) {
           throw new EvalException("as.character() returned non-character");
         }

tests/src/test/R/test.paste.R
@@ -42,3 +42,7 @@ test.paste.null <- function() {
     assertThat(paste(NULL), identicalTo(character(0)))
     assertThat(paste(NULL, collapse=""), identicalTo(""))
 }
+
+test.paste.eval <- function() {
+    assertThat(paste(quote(a)), identicalTo("a"))
+}
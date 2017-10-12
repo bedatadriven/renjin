50b33d3a8ab1128494007c9857ed4c970350d288
Alex Bertram <alex@bedatadriven.com>
2017-10-12 17:24:38 +0200

    Updates build to use new build of soot
    I've updated soot to support method handle invocations
    so we should be able to use it on all classes now.

--- tools/gcc-bridge/maven-plugin/src/main/java/org/renjin/gcc/maven/SootMojo.java
+++ tools/gcc-bridge/maven-plugin/src/main/java/org/renjin/gcc/maven/SootMojo.java
@@ -18,6 +18,7 @@
  */
 package org.renjin.gcc.maven;
 
+import com.google.common.io.Files;
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -54,9 +55,12 @@ public class SootMojo extends AbstractMojo {
   
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
-    
+
+    List<String> classes = new ArrayList<>();
     if(optimize == null || optimize.isEmpty()) {
-      throw new MojoFailureException("No classes to optimize.");
+      findClassfiles(new File(project.getBuild().getOutputDirectory()), "", classes);
+    } else {
+      classes.addAll(optimize);
     }
     
     List<String> args = new ArrayList<>();
@@ -77,13 +81,23 @@ public class SootMojo extends AbstractMojo {
     
     // Add classes to optimize
     args.add("-O");
-    args.addAll(optimize);
+    args.addAll(classes);
     
     // Write out to build directory and overwrite existing classfiles
     args.add("-d");
     args.add(project.getBuild().getOutputDirectory());
     
     soot.Main.main(args.toArray(new String[args.size()]));
+  }
+
+  private void findClassfiles(File file, String packageName, List<String> classes) {
+    for (File child : file.listFiles()) {
+      if(child.isFile() && child.getName().endsWith(".class")) {
+        classes.add(packageName + Files.getNameWithoutExtension(child.getName()));
+      } else if(child.isDirectory()) {
+        findClassfiles(child, packageName + child.getName() + ".", classes);
+      }
+    }
   }
 
   private String compileClassPath() throws MojoExecutionException {

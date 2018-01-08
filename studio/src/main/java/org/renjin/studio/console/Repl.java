46f632638a7e715fef45feb663758717d3a68339
Parham Solaimani <parham@bedatadriven.com>
2018-01-08 17:00:07 +0100

    update copyright year license and repl

--- studio/src/main/java/org/renjin/studio/console/Repl.java
+++ studio/src/main/java/org/renjin/studio/console/Repl.java
@@ -1,6 +1,6 @@
 /**
  * Renjin : JVM-based interpreter for the R language for the statistical analysis
- * Copyright © 2010-2016 BeDataDriven Groep B.V. and contributors
+ * Copyright © 2010-2018 BeDataDriven Groep B.V. and contributors
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -112,8 +112,8 @@ public class Repl implements Runnable {
     PrintStream out = console.getOut();
     out.print("Renjin " + RenjinVersion.getVersionName() + "\n");
 
-    out.print("Copyright (C) 2017 The R Foundation for Statistical Computing\n");
-    out.print("Copyright (C) 2017 BeDataDriven\n");
+    out.print("Copyright (C) 2018 The R Foundation for Statistical Computing\n");
+    out.print("Copyright (C) 2018 BeDataDriven\n");
 
   }
 

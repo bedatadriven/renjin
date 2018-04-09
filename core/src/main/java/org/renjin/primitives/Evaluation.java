9ac540ea5779400aac713bd01a1833ec430e9201
Parham Solaimani <parham@bedatadriven.com>
2018-04-09 16:06:35 +0200

    fix nargs() using incorrect context when passed as promise + test case

--- core/src/main/java/org/renjin/primitives/Evaluation.java
+++ core/src/main/java/org/renjin/primitives/Evaluation.java
@@ -493,8 +493,8 @@ public class Evaluation {
   }
 
   @Builtin
-  public static int nargs(@Current Context context) {
-    return context.getArguments().length();
+  public static int nargs(@Current Context context, @Current Environment environment) {
+    return Contexts.findCallingContext(context, environment).getArguments().length();
   }
   
   @Builtin(".Primitive")

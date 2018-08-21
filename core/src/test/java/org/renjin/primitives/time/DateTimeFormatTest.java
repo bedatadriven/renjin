0c1d5ded1fa274bd4f498bc553d63dfcdf4a0dd2
Alex Bertram <alex@bedatadriven.com>
2018-08-21 13:15:13 +0200

    Add support for %n in strptime() / format.POSIXxx()

--- core/src/test/java/org/renjin/primitives/time/DateTimeFormatTest.java
+++ core/src/test/java/org/renjin/primitives/time/DateTimeFormatTest.java
@@ -32,6 +32,11 @@ public class DateTimeFormatTest {
     verifyFormat("2009-07-01 00:00:00", "%Y-%m-%d %H:%M:%OS", new DateTime(2009,7,1,0,0,0));
   }
 
+  @Test
+  public void newlines() {
+    verifyFormat("Aug 21\n2018", "%b %d%n%Y", new DateTime(2018, 8, 21, 0, 0, 0));
+  }
+
 
   private void verifyFormat(String x, String format, DateTime dateTime) {
     DateTimeFormatter formatter = DateTimeFormat.forPattern(format);

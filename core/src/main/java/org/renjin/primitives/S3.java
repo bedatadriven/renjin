bedatadriven/renjin

commit 63bf94466d6695ca6adcef0cb26245294d6b6e6a

Fix handling of S3 default method selection

core/src/main/java/org/renjin/primitives/S3.java
@@ -1286,22 +1286,23 @@ public class S3 {
 
       if(next != null) {
         return next;
-      } else {
-        Environment methodTable = getMethodTable();
-        GenericMethod function = findNext(methodTable, genericMethodName, "default");
-        if(function != null) {
-          return function;
-        }
-
-        // as a last step, we call BACK into the primitive
-        // to get the default implementation  - ~ YECK ~
-        PrimitiveFunction primitive = Primitives.getBuiltin(genericMethodName);
-        if(primitive != null) {
-          return new GenericMethod(this, Symbol.get(genericMethodName + ".default"), null, primitive);
-        }
-
-        return null;
       }
+
+      // Look up the .default method in the calling environment
+      // and the original *definition environment*, *NOT* the methods table.
+      GenericMethod function = findNext(definitionEnvironment, genericMethodName, "default");
+      if(function != null) {
+        return function;
+      }
+
+      // as a last step, we call BACK into the primitive
+      // to get the default implementation  - ~ YECK ~
+      PrimitiveFunction primitive = Primitives.getBuiltin(genericMethodName);
+      if(primitive != null) {
+        return new GenericMethod(this, Symbol.get(genericMethodName + ".default"), null, primitive);
+      }
+
+      return null;
     }
 
     public GenericMethod findNext() {

test-packages/s3test/NAMESPACE
@@ -5,6 +5,8 @@ export(new.square)
 exportMethods(setCountryName, setCountryTemp, "[", "[[", "[[<-", Arith, "/")
 exportClasses(Country, City)
 
+importFrom(grid, makeContext)
+
 # export extensions of methods in other packages
 S3method(as.character, circle)
 S3method(as.character, square)
@@ -14,3 +16,5 @@ S3method(area, circle)
 S3method(area, square)
 
 S3method(`%/%`, circle)
+
+S3method(makeContext, foo)

test-packages/s3test/pom.xml
@@ -30,6 +30,11 @@
             <artifactId>stats</artifactId>
             <version>${project.version}</version>
         </dependency>
+        <dependency>
+            <groupId>org.renjin</groupId>
+            <artifactId>grid</artifactId>
+            <version>${project.version}</version>
+        </dependency>
     </dependencies>
     <build>
         <plugins>

test-packages/s3test/src/main/R/makeContext.R
@@ -1,0 +1,23 @@
+#
+# Renjin : JVM-based interpreter for the R language for the statistical analysis
+# Copyright © 2010-2018 BeDataDriven Groep B.V. and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, a copy is available at
+# https://www.gnu.org/licenses/gpl-2.0.txt
+#
+
+
+makeContext.foo <- function(x) {
+    list(result = NextMethod())
+}

test-packages/s3test/src/test/R/test.NextMethod.default.R
@@ -1,0 +1,29 @@
+#
+# Renjin : JVM-based interpreter for the R language for the statistical analysis
+# Copyright © 2010-2018 BeDataDriven Groep B.V. and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, a copy is available at
+# https://www.gnu.org/licenses/gpl-2.0.txt
+#
+
+library(methods)
+library("org.renjin.test:s3test")
+
+# This is a test to ensure that NextMethod() looks for the .default method NOT
+# in the S3 method table of the grid package, but in the grid package's namespace.
+
+x <- structure(42, class = "foo")
+
+stopifnot(grid::makeContext(x) == 42)
+

commit 08207cee484e9a4f80465b67d8311453a2eecd34

Fix regression in S3 dispatch caused by 63bf94466d6695ca6adcef0cb26245294d6b6e6a
When searching for default functions, we need to look in *both*
the definition environment, *and* the definition environment's method
table, in case *another* package has provided a default method.

core/src/main/java/org/renjin/primitives/S3.java
@@ -623,9 +623,14 @@ public class S3 {
         return next;
       }
 
-      // Look up the .default method in the calling environment
-      // and the original *definition environment*, *NOT* the methods table.
+      // Look up the .default method first in the definition environment
       GenericMethod function = findNext(definitionEnvironment, genericMethodName, "default");
+      if(function != null) {
+        return function;
+      }
+
+      // Otherwise see if *another* package has defined a default method
+      function = findNext(getMethodTable(), genericMethodName, "default");
       if(function != null) {
         return function;
       }
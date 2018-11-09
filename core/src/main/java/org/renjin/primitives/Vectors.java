bedatadriven/renjin

commit 56269ea4362a2f46a7d96544768e09b925b8be51

Fix for Rf_coerceVector()
An expected side effect of this function is that the resulting
vector is safely mutable, so we can't defer the conversion: we need
to allocate an array for the vector immediately.

tools/gnur-runtime/src/main/java/org/renjin/gnur/api/Rinternals.java
@@ -1164,6 +1164,10 @@ public final class Rinternals {
     return GnuCharSexp.valueOf(((AtomicVector) p0).getElementAsString(0));
   }
 
+  /**
+   * Coerces a vector from one type to another. Calling code expects to be
+   * able to safely modify the resulting vector.
+   */
   public static SEXP Rf_coerceVector(SEXP p0, /*SEXPTYPE*/ int type) {
     switch(type) {
       case SexpType.LISTSXP:
@@ -1171,9 +1175,9 @@ public final class Rinternals {
       case SexpType.LGLSXP:
         return Vectors.asLogical(((Vector) p0)).setAttributes(p0.getAttributes());
       case SexpType.INTSXP:
-        return Vectors.asInteger((Vector)p0).setAttributes(p0.getAttributes());
+        return new IntArrayVector((AtomicVector)p0);
       case SexpType.REALSXP:
-        return Vectors.asDouble((Vector)p0).setAttributes(p0.getAttributes());
+        return new DoubleArrayVector((AtomicVector) p0);
       case SexpType.CPLXSXP:
         return Vectors.asComplex((Vector)p0).setAttributes(p0.getAttributes());
       case SexpType.STRSXP:

core/src/main/java/org/renjin/sexp/DoubleArrayVector.java
@@ -81,9 +81,10 @@ public final class DoubleArrayVector extends DoubleVector {
   }
 
   /**
-   * Creates a new DoubleArrayVector that is a copy of the given {@code vector}
+   * Creates a new DoubleArrayVector that is a copy of the given {@code vector},
+   * with the same attributes.
    */
-  public DoubleArrayVector(DoubleVector vector) {
+  public DoubleArrayVector(AtomicVector vector) {
     super(vector.getAttributes());
     this.values = vector.toDoubleArray();
   }

core/src/main/java/org/renjin/sexp/IntArrayVector.java
@@ -36,7 +36,11 @@ public class IntArrayVector extends IntVector {
     this.values = Arrays.copyOf(values, values.length);
   }
 
-  public IntArrayVector(IntVector vector) {
+  /**
+   * Creates a new IntArrayVector that is a copy of the given {@code vector},
+   * with the same attributes.
+   */
+  public IntArrayVector(AtomicVector vector) {
     super(vector.getAttributes());
     this.values = vector.toIntArray();
   }

commit 0013c14a7022b0910263433836c3027bd8a35efc

Correct regression to Rf_coerceVector introduced by 56269ea
Rf_coerceVector() must accept list vectors as well as atomic vectors.

core/src/main/java/org/renjin/primitives/Vectors.java
@@ -284,7 +284,7 @@ public class Vectors {
     return asRaw(source.toVector());
   }
 
-  static Vector convertToAtomicVector(Vector.Builder builder, Vector source) {
+  public static Vector convertToAtomicVector(Vector.Builder builder, Vector source) {
     if(source instanceof ListVector) {
       for (int i = 0; i != source.length(); ++i) {
         SEXP value = ((ListVector) source).getElementAsSEXP(i);
@@ -434,7 +434,7 @@ public class Vectors {
    * @param x
    * @param mode
    */
-  private static void checkForListThatCannotBeCoercedToAtomicVector(Vector x, String mode) {
+  public static void checkForListThatCannotBeCoercedToAtomicVector(Vector x, String mode) {
     if(x instanceof ListVector) {
       ListVector list = (ListVector) x;
       for (int i = 0; i < list.length(); i++) {

tools/gnur-runtime/src/main/java/org/renjin/gnur/api/Rinternals.java
@@ -1175,9 +1175,9 @@ public final class Rinternals {
       case SexpType.LGLSXP:
         return Vectors.asLogical(((Vector) p0)).setAttributes(p0.getAttributes());
       case SexpType.INTSXP:
-        return new IntArrayVector((AtomicVector)p0);
+        return asIntArrayVector((Vector)p0);
       case SexpType.REALSXP:
-        return new DoubleArrayVector((AtomicVector) p0);
+        return asDoubleArrayVector((Vector) p0);
       case SexpType.CPLXSXP:
         return Vectors.asComplex((Vector)p0).setAttributes(p0.getAttributes());
       case SexpType.STRSXP:
@@ -1186,6 +1186,22 @@ public final class Rinternals {
         return toExpressionList(p0);
     }
     throw new UnimplementedGnuApiMethod("Rf_coerceVector: " + type);
+  }
+
+  private static SEXP asIntArrayVector(Vector vector) {
+    Vectors.checkForListThatCannotBeCoercedToAtomicVector(vector, "integer");
+
+    IntArrayVector.Builder builder = new IntArrayVector.Builder(0, vector.length());
+    Vector integerVector = Vectors.convertToAtomicVector(builder, vector);
+    return integerVector.setAttributes(vector.getAttributes());
+  }
+
+  private static SEXP asDoubleArrayVector(Vector vector) {
+    Vectors.checkForListThatCannotBeCoercedToAtomicVector(vector, "double");
+
+    DoubleArrayVector.Builder builder = new DoubleArrayVector.Builder(0, vector.length());
+    Vector integerVector = Vectors.convertToAtomicVector(builder, vector);
+    return integerVector.setAttributes(vector.getAttributes());
   }
 
   private static SEXP toExpressionList(SEXP sexp) {
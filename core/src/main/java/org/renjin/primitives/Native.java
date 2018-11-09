bedatadriven/renjin

commit 9422876e8e6d4e686b5823ca46f1337bec4164b4

GCC-bridge pointer fixes, strspn() implementation

core/src/main/java/org/renjin/primitives/Native.java
@@ -22,10 +22,7 @@ import org.renjin.base.Base;
 import org.renjin.eval.Context;
 import org.renjin.eval.EvalException;
 import org.renjin.eval.Profiler;
-import org.renjin.gcc.runtime.BytePtr;
-import org.renjin.gcc.runtime.DoublePtr;
-import org.renjin.gcc.runtime.IntPtr;
-import org.renjin.gcc.runtime.ObjectPtr;
+import org.renjin.gcc.runtime.*;
 import org.renjin.invoke.annotations.*;
 import org.renjin.invoke.reflection.ClassBindingImpl;
 import org.renjin.invoke.reflection.FunctionBinding;
@@ -217,7 +214,7 @@ public class Native {
   /**
    * Converts a StringVector to an array of null-terminated strings.
    */
-  private static ObjectPtr stringPtrToCharPtrPtr(SEXP sexp) {
+  private static PointerPtr stringPtrToCharPtrPtr(SEXP sexp) {
     if(!((sexp instanceof StringVector))) {
       throw new EvalException(".C function expected 'character', but argument was '%s'", sexp.getTypeName());
     }
@@ -229,7 +226,7 @@ public class Native {
         strings[i] = BytePtr.nullTerminatedString(element, Charsets.UTF_8);
       }
     }
-    return new ObjectPtr(strings, 0);
+    return new PointerPtr(strings, 0);
   }
 
   private static SEXP sexpFromPointer(Object ptr, AttributeMap attributes) {

tools/gcc-bridge/runtime/src/main/java/org/renjin/gcc/runtime/ObjectPtr.java
@@ -96,7 +96,7 @@ public class ObjectPtr<T> extends AbstractPtr {
 
   @Override
   public int toInt() {
-    throw new UnsupportedOperationException("TODO");
+    return offset * 4;
   }
 
   @Override

tools/gcc-bridge/runtime/src/main/java/org/renjin/gcc/runtime/Stdlib.java
@@ -180,6 +180,31 @@ public class Stdlib {
     return new OffsetPtr(string, offset);
   }
 
+  /**
+   * Scans str1 for the first occurrence of any of the characters that are part of str2,
+   * returning the number of characters of str1 read before this first occurrence.
+   *
+   * The search includes the terminating null-characters. Therefore, the function will return the
+   * length of str1 if none of the characters of str2 are found in str1.
+   */
+  public static int strcspn(Ptr str1, Ptr str2) {
+    int i = 0;
+    byte c, d;
+    while(true) {
+      c = str1.getByte(i);
+      int j = 0;
+      do {
+        d = str2.getByte(j);
+        if(c == d) {
+          return i;
+        }
+        j++;
+      } while(d != 0);
+      i++;
+    }
+  }
+
+
   @Deprecated
   public static long strtol(Ptr string) {
     return strtol(string, BytePtr.NULL, 10);
@@ -554,10 +579,19 @@ public class Stdlib {
    * Returns the time since the Epoch (00:00:00 UTC, January 1, 1970), measured in seconds.
    * If seconds is not NULL, the return value is also stored in variable seconds.
    */
+  @Deprecated
   public static int time(IntPtr seconds) {
+    return time((Ptr)seconds);
+  }
+
+  /**
+   * Returns the time since the Epoch (00:00:00 UTC, January 1, 1970), measured in seconds.
+   * If seconds is not NULL, the return value is also stored in variable seconds.
+   */
+  public static int time(Ptr seconds) {
     int time = (int) (System.currentTimeMillis() / 1000L);
-    if(seconds.array != null) {
-      seconds.array[seconds.offset] = time;
+    if(!seconds.isNull()) {
+      seconds.setInt(time);
     }
     return time;
   }

tools/gcc-bridge/runtime/src/test/java/org/renjin/gcc/runtime/StdlibTest.java
@@ -72,6 +72,16 @@ public class StdlibTest {
     assertThat(Stdlib.strtoul(cstring("0xfffffffffffffffe"), NULL, 0), equalTo(0xfffffffffffffffeL));
   }
 
+  @Test
+  public void strcspn() {
+    Ptr x = BytePtr.nullTerminatedString("hello world", StandardCharsets.US_ASCII);
+    Ptr y = BytePtr.nullTerminatedString("MXo", StandardCharsets.US_ASCII);
+    Ptr z = BytePtr.nullTerminatedString("QP", StandardCharsets.US_ASCII);
+
+    assertThat(Stdlib.strcspn(x, y), equalTo(4));
+    assertThat(Stdlib.strcspn(x, z), equalTo(Stdlib.strlen(x)));
+  }
+
   private BytePtr cstring(String str) {
     return BytePtr.nullTerminatedString(str, StandardCharsets.UTF_8);
   }

commit 2530776d7483b67890840e59c3ea869b4b8ef5b2

Fix regression in .C string handling introduced by 9422876e8e

core/src/main/java/org/renjin/primitives/Native.java
@@ -22,7 +22,10 @@ import org.renjin.base.Base;
 import org.renjin.eval.Context;
 import org.renjin.eval.EvalException;
 import org.renjin.eval.Profiler;
-import org.renjin.gcc.runtime.*;
+import org.renjin.gcc.runtime.BytePtr;
+import org.renjin.gcc.runtime.DoublePtr;
+import org.renjin.gcc.runtime.IntPtr;
+import org.renjin.gcc.runtime.PointerPtr;
 import org.renjin.invoke.annotations.*;
 import org.renjin.invoke.reflection.ClassBindingImpl;
 import org.renjin.invoke.reflection.FunctionBinding;
@@ -206,7 +209,7 @@ public class Native {
       }
       builder.add(callArguments.getName(i), sexpFromPointer(
           nativeArguments[i],
-          callArguments.get(i).getAttributes()));
+          callArguments.get(i)));
     }
     return builder.build();
   }
@@ -229,15 +232,15 @@ public class Native {
     return new PointerPtr(strings, 0);
   }
 
-  private static SEXP sexpFromPointer(Object ptr, AttributeMap attributes) {
+  private static SEXP sexpFromPointer(Object ptr, SEXP inputArgument) {
     // We are trusting the C code not to modify the arrays after the call
     // returns. 
     if(ptr instanceof DoublePtr) {
-      return DoubleArrayVector.unsafe(((DoublePtr) ptr).array, attributes);
+      return DoubleArrayVector.unsafe(((DoublePtr) ptr).array, inputArgument.getAttributes());
     } else if(ptr instanceof IntPtr) {
-      return new IntArrayVector(((IntPtr) ptr).array, attributes);
-    } else if(ptr instanceof ObjectPtr) {
-      return new NativeStringVector((ObjectPtr)ptr, attributes);
+      return new IntArrayVector(((IntPtr) ptr).array, inputArgument.getAttributes());
+    } else if(ptr instanceof PointerPtr) {
+      return new NativeStringVector((PointerPtr) ptr, inputArgument.getAttributes());
     } else {
       throw new UnsupportedOperationException(ptr.toString());
     }

core/src/main/java/org/renjin/primitives/NativeStringVector.java
@@ -18,18 +18,20 @@
  */
 package org.renjin.primitives;
 
-import org.renjin.gcc.runtime.BytePtr;
 import org.renjin.gcc.runtime.ObjectPtr;
+import org.renjin.gcc.runtime.PointerPtr;
+import org.renjin.gcc.runtime.Ptr;
+import org.renjin.gcc.runtime.Stdlib;
 import org.renjin.sexp.AttributeMap;
 import org.renjin.sexp.StringVector;
 
 public class NativeStringVector extends StringVector {
   
-  private BytePtr[] array;
+  private Ptr[] array;
   private int offset;
   private int length;
 
-  public NativeStringVector(BytePtr[] array, int offset, int length, AttributeMap attributes) {
+  public NativeStringVector(Ptr[] array, int offset, int length, AttributeMap attributes) {
     super(attributes);
     this.array = array;
     this.offset = offset;
@@ -38,9 +40,16 @@ public class NativeStringVector extends StringVector {
 
   public NativeStringVector(ObjectPtr ptr, AttributeMap attributes) {
     super(attributes);
-    this.array = (BytePtr[])ptr.array;
+    this.array = (Ptr[])ptr.array;
     this.offset = ptr.offset;
     this.length = ptr.array.length;
+  }
+
+  public NativeStringVector(PointerPtr ptr, AttributeMap attributes) {
+    super(attributes);
+    this.array = (Ptr[]) ptr.getArray();
+    this.offset = 0;
+    this.length = this.array.length;
   }
 
   @Override
@@ -55,11 +64,11 @@ public class NativeStringVector extends StringVector {
 
   @Override
   public String getElementAsString(int index) {
-    BytePtr string = array[offset + index];
+    Ptr string = array[offset + index];
     if(string == null) {
       return null;
     } else {
-      return string.nullTerminatedString();
+      return Stdlib.nullTerminatedString(string);
     }
   }
 
c3c72d68a6b80877259a963366a89a2269fbb262
Metin Yazici <25015317+strboul@users.noreply.github.com>
2019-03-15 15:33:56 +0100

    Fix inline function

--- core/src/main/R/base/strwrap.R
+++ core/src/main/R/base/strwrap.R
@@ -196,7 +196,9 @@ trimws <- function(x, which = c("both", "left", "right"),
                    whitespace = "[ \t\r\n]")
 {
     which <- match.arg(which)
-    mysub <- function(re, x) sub(re, "", x, perl = TRUE)
+    mysub <- function(re, x) {
+        sub(re, "", x, perl = TRUE)
+    }
     switch(which,
            "left" = mysub(paste0("^", whitespace, "+"), x),
            "right"= mysub(paste0(whitespace, "+$"), x),

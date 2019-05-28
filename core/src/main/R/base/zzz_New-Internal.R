7194d0359e614e0148e0271b395dd7776966d419
Maarten-Jan Kallen <4612238+mjkallen@users.noreply.github.com>
2019-05-28 15:44:22 +0200

    Update New-Internal.R to R version 3.5.3
    This also improves Renjin's implementation of nchar() to better match
    R's implementation. In particular, it now correctly handles the 'keepNA'
    argument.

---
+++ core/src/main/R/base/zzz_New-Internal.R
@@ -0,0 +1,54 @@
+
+gc <- function(verbose = getOption("verbose"),	reset=FALSE) {
+
+    # This is implemented as a NO OP in Renjin.
+    # While it is possible to force the JVM to stop and run
+    # garbage collection, it is very UNLIKELY to yield any
+    # performance benefit because the JVM does its job very well
+    # without intervention, and generally in parallel, while calling
+    # Runtime.gc() stops the world to run an exhaustive collection.
+
+    res <- matrix(numeric(14), 2L, 7L,
+		  dimnames = list(c("Ncells","Vcells"),
+		  c("used", "(Mb)", "gc trigger", "(Mb)",
+		    "limit (Mb)", "max used", "(Mb)")))
+    if(all(is.na(res[, 5L]))) res[, -5L] else res
+}
+
+gcinfo <- function(verbose) FALSE
+
+gctorture <- function(on=TRUE) {
+    warning("Renjin does not implement the gc torture mode.")
+    FALSE
+}
+
+gctorture2 <- function(step, wait = step, inhibit_release = FALSE) {
+    warning("Renjin does not implement the gc torture mode.")
+    FALSE
+}
+
+# overwrite l10n_info in version 3.5.3, because it calls the internal l10n_info:
+l10n_info <- function() {
+    list(MBCS = TRUE, `UTF-8` = TRUE, `Latin-1` = FALSE)
+}
+
+
+# lengths
+# Introduced in R-3.2.0, see the release notes at http://cran.r-project.org/src/base/NEWS
+# Fixed in R-3.2.1 to work (trivially) on atomic vectors.
+lengths <- function(x, use.names = TRUE) {
+    if (!isTRUE(use.names)) x <- unname(x)
+    sapply(x, length)
+}
+
+nchar <- function(x, type = "chars", allowNA = FALSE, keepNA = NA) {
+    if (is.factor(x)) stop("'nchar()' requires a character vector")
+
+    res <- .Internal(nchar(x, type, allowNA))
+
+    if ((is.na(keepNA) || isTRUE(keepNA)) && anyNA(x)) {
+        res[which(is.na(x))] <- NA_integer_
+    }
+
+    res
+}

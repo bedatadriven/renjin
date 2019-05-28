6648a8cfa81aa72339a5f43e7e8886f3d7c21484
Maarten-Jan Kallen <4612238+mjkallen@users.noreply.github.com>
2019-05-28 12:07:00 +0200

    Update rep.R to R version 3.5.3

--- core/src/main/R/base/renjinStubs.R
+++ core/src/main/R/base/renjinStubs.R
@@ -1,10 +1,4 @@
 
-# rep_len
-# Introduced in R-3.0.0, see the release notes at http://cran.r-project.org/src/base/NEWS
-rep_len <- function(x, length.out) {
-    rep(x, length.out = length.out)
-}
-
 # lengths
 # Introduced in R-3.2.0, see the release notes at http://cran.r-project.org/src/base/NEWS
 # Fixed in R-3.2.1 to work (trivially) on atomic vectors.
---
+++ core/src/main/R/base/zzz_rep.R
@@ -0,0 +1,6 @@
+
+# rep_len
+# Introduced in R-3.0.0, see the release notes at http://cran.r-project.org/src/base/NEWS
+rep_len <- function(x, length.out) {
+    rep(x, length.out = length.out)
+}

0a5d3719181ede57f5a44a3a0c4cc15eb0753212
Alex Bertram <alex@bedatadriven.com>
2019-07-25 16:38:36 +0200

    Provide a stub for installed.packages() [knitr]

--- packages/utils/R/zzzz-renjin.R
+++ packages/utils/R/zzzz-renjin.R
@@ -5,3 +5,23 @@
 flush.console <- function() invisible()
 
 process.events <- function() invisible()
+
+installed.packages <- function (lib.loc = NULL, priority = NULL, noCache = FALSE, fields = NULL,
+                          subarch = .Platform$r_arch, ...)
+
+{
+    pkgs <- c("base", "compiler", "datasets", "graphics", "grDevices",  "grid", "methods", "parallel", "splines", "stats", "stats4",  "tcltk", "tools", "utils")
+    pkgmatrix <- cbind(
+        Package = pkgs,
+        LibPath = sprintf("classpath:///org/renjin/%s", pkgs),
+        Version = "3.5.3",
+        Priority = "base",
+        Depends = NA,
+        Imports = NA,
+        LinkingTo = NA,
+        Suggests = NA,
+        Enhances = NA)
+
+    rownames(pkgmatrix) <- pkgs
+    pkgmatrix
+}

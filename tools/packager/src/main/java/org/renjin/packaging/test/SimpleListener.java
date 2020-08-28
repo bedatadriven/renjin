c6b222b170c26608c6882856bf6ee247c45f056f
per <per@alipsa.se>
2020-08-28 13:09:07 +0200

    fix failing test in renjin, add abilibty to output test results, set proper ownership of /home/vagrant/.gradle in the vagrant setup

--- tools/packager/src/main/java/org/renjin/packaging/test/SimpleListener.java
+++ tools/packager/src/main/java/org/renjin/packaging/test/SimpleListener.java
@@ -1,6 +1,9 @@
 package org.renjin.packaging.test;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,8 +13,10 @@ class SimpleListener implements TestListener {
   private String executingTestName;
   private int passCount;
   private int failCount;
+  private boolean countTimoutAsError;
 
-  private List<String> failedCases = new ArrayList<>();
+  private final List<String> failedCases = new ArrayList<>();
+  private final List<String> passCases = new ArrayList<>();
 
   @Override
   public void debug(String message) {
@@ -28,9 +33,18 @@ class SimpleListener implements TestListener {
     this.executingTestName = testName;
   }
 
+  public SimpleListener() {
+    this(false);
+  }
+
+  public SimpleListener(boolean countTimoutAsError) {
+    this.countTimoutAsError = countTimoutAsError;
+  }
+
   @Override
   public void pass() {
     passCount++;
+    passCases.add(testFile + " " + executingTestName);
   }
 
   @Override
@@ -47,6 +61,10 @@ class SimpleListener implements TestListener {
   @Override
   public void timeout() {
     System.err.println(testFile + " " + executingTestName + " timed out.");
+    if (countTimoutAsError) {
+      failCount++;
+      failedCases.add(testFile + " " + executingTestName);
+    }
   }
 
   public int getFailCount() {
@@ -62,5 +80,22 @@ class SimpleListener implements TestListener {
       System.err.println();
     }
     System.err.printf("Tests complete: %d/%d passed.%n", passCount, passCount + failCount);
+  }
+
+  public void saveResults(File reportDir) {
+    // ReportDir is required and always created by the TestExecutor so no need to check for null and non existing dirs
+    File resultFile = new File(reportDir,"renjin-test-results.log");
+    try(Writer writer = new FileWriter(resultFile)) {
+      for (String passCase : passCases) {
+        writer.write( passCase + " pass\n");
+      }
+      for (String failCase : failedCases) {
+        writer.write(failCase + " fail\n");
+      }
+      writer.flush();
+    } catch (IOException e) {
+      System.err.println("Failed to save test results to file " + resultFile.getAbsolutePath());
+      e.printStackTrace(System.err);
+    }
   }
 }

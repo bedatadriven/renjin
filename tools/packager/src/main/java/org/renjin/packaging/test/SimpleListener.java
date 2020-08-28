7ebef7600fe0f0c5caf5d6aa7d9e8fcdb69e1196
per <per@alipsa.se>
2020-08-28 14:38:32 +0200

    capture test results in an object for easier formatting later if needed.

--- tools/packager/src/main/java/org/renjin/packaging/test/SimpleListener.java
+++ tools/packager/src/main/java/org/renjin/packaging/test/SimpleListener.java
@@ -9,14 +9,16 @@ import java.util.List;
 
 class SimpleListener implements TestListener {
 
+  public static final String RESULTS_FILE_NAME = "renjin-test-results.log";
+
   private String testFile = "";
   private String executingTestName;
   private int passCount;
   private int failCount;
-  private boolean countTimoutAsError;
+  private final boolean countTimoutAsError;
 
-  private final List<String> failedCases = new ArrayList<>();
-  private final List<String> passCases = new ArrayList<>();
+  private final List<TestResult> failedCases = new ArrayList<>();
+  private final List<TestResult> passCases = new ArrayList<>();
 
   @Override
   public void debug(String message) {
@@ -44,13 +46,13 @@ class SimpleListener implements TestListener {
   @Override
   public void pass() {
     passCount++;
-    passCases.add(testFile + " " + executingTestName);
+    passCases.add(new TestResult(testFile, executingTestName));
   }
 
   @Override
   public void fail() {
     failCount++;
-    failedCases.add(testFile + " " + executingTestName);
+    failedCases.add(new TestResult(testFile, executingTestName));
     System.err.println(testFile + " " + executingTestName + " failed.");
   }
 
@@ -63,7 +65,7 @@ class SimpleListener implements TestListener {
     System.err.println(testFile + " " + executingTestName + " timed out.");
     if (countTimoutAsError) {
       failCount++;
-      failedCases.add(testFile + " " + executingTestName);
+      failedCases.add(new TestResult(testFile, executingTestName));
     }
   }
 
@@ -74,7 +76,7 @@ class SimpleListener implements TestListener {
   public void printResults() {
     if(!failedCases.isEmpty()) {
       System.err.println("Failed test cases:");
-      for (String failedCase : failedCases) {
+      for (TestResult failedCase : failedCases) {
         System.err.println("  " + failedCase);
       }
       System.err.println();
@@ -84,18 +86,38 @@ class SimpleListener implements TestListener {
 
   public void saveResults(File reportDir) {
     // ReportDir is required and always created by the TestExecutor so no need to check for null and non existing dirs
-    File resultFile = new File(reportDir,"renjin-test-results.log");
+    File resultFile = new File(reportDir,RESULTS_FILE_NAME);
     try(Writer writer = new FileWriter(resultFile)) {
-      for (String passCase : passCases) {
+      for (TestResult passCase : passCases) {
         writer.write( passCase + " pass\n");
       }
-      for (String failCase : failedCases) {
+      for (TestResult failCase : failedCases) {
         writer.write(failCase + " fail\n");
       }
       writer.flush();
     } catch (IOException e) {
       System.err.println("Failed to save test results to file " + resultFile.getAbsolutePath());
       e.printStackTrace(System.err);
+    }
+  }
+
+  /**
+   * Simple class to capture test results.
+   * This makes it easier to change the format of the test file later since we can differentiate between
+   * the test file and the test in case a more structured format is needed (e.g. json, yaml, xml)
+   */
+  private static class TestResult {
+    String testName;
+    String testFile;
+
+    TestResult(String testFile, String testName) {
+      this.testFile = testFile;
+      this.testName = testName;
+    }
+
+    @Override
+    public String toString() {
+      return testFile + " " + testName;
     }
   }
 }
